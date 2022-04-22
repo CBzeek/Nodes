d835dd32346d85c29d6bd474620d9d33c7e5ec49
CBzeek <60964131+CBzeek@users.noreply.github.com>
2022-04-22 11:26:27 +0300

    Update aptos-del-wo-key.sh

--- aptos-del-wo-key.sh
+++ aptos-del-wo-key.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd $HOME
 systemctl stop aptosd
-mkdir ~/.aptos-backup-key/
+mkdir -p ~/.aptos-backup-key/
 cp ~/.aptos/key/private-key.txt ~/.aptos-backup-key/private-key.txt
 rm -rf ~/aptos-core
 rm -rf ~/.aptos/config/
