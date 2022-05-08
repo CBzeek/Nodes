b33cfbdb97f40ad6650ae75f751e4427b03b8291
CBzeek <60964131+CBzeek@users.noreply.github.com>
2022-05-08 13:19:02 +0300

    Add files via upload

---
+++ aptos-seed-add.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+echo 'Stopping aptos node...'
+sudo systemctl stop aptosd
+echo 'Done'
+sleep 2
+
+cd $HOME
+
+echo 'Backup configuration files...'
+cp -r $HOME/.aptos $HOME/aptos_backup_$(date +%F--%R)
+echo 'Done'
+sleep 2
+
+
+echo 'Check for yq...'
+if [ -f /usr/local/bin/yq ]; then
+    echo 'yq installed'
+else 
+    echo 'yq not stalled, starting install...'
+    wget -q -O /usr/local/bin/yq https://github.com/mikefarah/yq/releases/download/v4.23.1/yq_linux_amd64 && chmod +x /usr/local/bin/yq
+    echo 'Done'
+fi
+
+echo 'Add seeds to configuration file...'
+wget -O $HOME/seeds.yaml https://raw.githubusercontent.com/Pa1amar/aptos/main/seeds.yaml
+/usr/local/bin/yq ea -i 'select(fileIndex==0).full_node_networks[0].seeds = select(fileIndex==1).seeds | select(fileIndex==0)' $HOME/.aptos/config/public_full_node.yaml $HOME/seeds.yaml
+echo 'Done'
+sleep 2
+
+echo 'Stopping aptos node...'
+sudo systemctl restart aptosd
+echo 'Done'
+sleep 2
+
