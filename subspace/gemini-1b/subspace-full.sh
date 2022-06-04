1b40cf2b73fa1abfc8aa23ce29d5c08e5a9ecf17
CBzeek <60964131+CBzeek@users.noreply.github.com>
2022-06-04 08:52:49 +0300

    Update subspace-full.sh

--- subspace/gemini-1b/subspace-full.sh
+++ subspace/gemini-1b/subspace-full.sh
@@ -13,8 +13,6 @@ bash_profile=$HOME/.bash_profile
 if [ -f "$bash_profile" ]; then
     . $HOME/.bash_profile
 fi
-sleep 1 && curl -s https://api.nodes.guru/logo.sh | bash && sleep 1
-
 
 cd $HOME
 rm -rf subspace*
@@ -36,7 +34,7 @@ After=network.target
 [Service]
 User=$USER
 Type=simple
-ExecStart=$(which subspace-node) --chain gemini-1 --execution wasm --keep-blocks 1024 --pruning archive --validator --name $SUBSPACE_NODENAME
+ExecStart=$(which subspace-node) --chain gemini-1 --pruning 1024 --execution wasm --keep-blocks 1024 --validator --name $SUBSPACE_NODENAME
 Restart=on-failure
 LimitNOFILE=65535
 
@@ -72,7 +70,6 @@ echo -e '\n\e[42mCheck node status\e[0m\n' && sleep 1
 if [[ `service subspaced status | grep active` =~ "running" ]]; then
   echo -e "Your Subspace node \e[32minstalled and works\e[39m!"
   echo -e "You can check node status by the command \e[7mservice subspaced status\e[0m"
-  echo -e "Press \e[7mQ\e[0m for exit from status menu"
 else
   echo -e "Your Subspace node \e[31mwas not installed correctly\e[39m, please reinstall."
 fi
@@ -82,7 +79,6 @@ echo -e '\n\e[42mCheck farmer status\e[0m\n' && sleep 1
 if [[ `service subspaced-farmer status | grep active` =~ "running" ]]; then
   echo -e "Your Subspace farmer \e[32minstalled and works\e[39m!"
   echo -e "You can check node status by the command \e[7mservice subspaced-farmer status\e[0m"
-  echo -e "Press \e[7mQ\e[0m for exit from status menu"
 else
   echo -e "Your Subspace farmer \e[31mwas not installed correctly\e[39m, please reinstall."
 fi
