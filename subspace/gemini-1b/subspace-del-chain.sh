1bc8746c5f3f975a9da77f5e6946b327ea4e3164
CBzeek <60964131+CBzeek@users.noreply.github.com>
2022-06-04 10:43:21 +0300

    Update subspace-del-chain.sh

--- subspace/gemini-1b/subspace-del-chain.sh
+++ subspace/gemini-1b/subspace-del-chain.sh
@@ -10,7 +10,7 @@ echo '### Erase chain data'
 subspace-farmer wipe
 sleep 2
 #subspace-node purge-chain --chain gemini-1 -y > /dev/null 2>&1
-subspace-node purge-chain --chain gemini-1
+subspace-node purge-chain --chain gemini-1 -y
 sleep 2
 
 echo '### Starting subspace service'
