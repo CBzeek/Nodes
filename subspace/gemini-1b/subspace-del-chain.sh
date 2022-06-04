b4c8ccaa4e742e780b0f45a97538952700c7d380
CBzeek <60964131+CBzeek@users.noreply.github.com>
2022-06-04 10:43:48 +0300

    Update subspace-del-chain.sh

--- subspace/gemini-1b/subspace-del-chain.sh
+++ subspace/gemini-1b/subspace-del-chain.sh
@@ -2,11 +2,13 @@
 cd $HOME
 
 echo '### Stopping subspace service'
+echo ''
 #sudo systemctl stop subspaced
 #sudo systemctl stop subspaced-farmer 
 sudo systemctl stop subspaced subspaced-farmer 
 
 echo '### Erase chain data'
+echo ''
 subspace-farmer wipe
 sleep 2
 #subspace-node purge-chain --chain gemini-1 -y > /dev/null 2>&1
@@ -14,6 +16,7 @@ subspace-node purge-chain --chain gemini-1 -y
 sleep 2
 
 echo '### Starting subspace service'
+echo ''
 sudo systemctl restart subspaced subspaced-farmer
 sleep 2
 
