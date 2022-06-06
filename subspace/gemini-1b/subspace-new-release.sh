57233283464a8c1c0a58011919e35823f0ae1917
CBzeek <60964131+CBzeek@users.noreply.github.com>
2022-06-06 10:03:45 +0300

    Create subspace-new-release.sh

---
+++ subspace/gemini-1b/subspace-new-release.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+cd $HOME
+
+echo '### Stopping subspace service'
+echo ''
+sudo systemctl stop subspaced subspaced-farmer 
+
+wget -O subspace-node https://github.com/subspace/subspace/releases/download/gemini-1b-2022-june-05/subspace-node-ubuntu-x86_64-gemini-1b-2022-june-05 
+wget -O subspace-farmer https://github.com/subspace/subspace/releases/download/gemini-1b-2022-june-05/subspace-farmer-ubuntu-x86_64-gemini-1b-2022-june-05
+chmod +x subspace*
+mv subspace* /usr/local/bin/
+
+echo '### Starting subspace node service'
+echo ''
+sudo systemctl restart subspaced
+sleep 10
+
+echo '### Starting subspace farmer service'
+echo ''
+sudo systemctl restart subspaced-farmer
