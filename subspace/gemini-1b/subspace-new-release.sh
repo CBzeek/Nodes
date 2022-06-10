15078134b767b4574abf6d1951e98e0a9734546f
CBzeek <60964131+CBzeek@users.noreply.github.com>
2022-06-10 23:24:57 +0300

    Update subspace-new-release.sh

--- subspace/gemini-1b/subspace-new-release.sh
+++ subspace/gemini-1b/subspace-new-release.sh
@@ -13,8 +13,8 @@ sudo systemctl enable subspaced subspaced-farmer
 
 echo '### Download new Subspace release'
 echo ''
-wget -O subspace-node https://github.com/subspace/subspace/releases/download/gemini-1b-2022-june-05/subspace-node-ubuntu-x86_64-gemini-1b-2022-june-05
-wget -O subspace-farmer https://github.com/subspace/subspace/releases/download/gemini-1b-2022-june-05/subspace-farmer-ubuntu-x86_64-gemini-1b-2022-june-05
+wget -O subspace-node https://github.com/subspace/subspace/releases/download/gemini-1b-2022-jun-10/subspace-node-ubuntu-x86_64-gemini-1b-2022-jun-10
+wget -O subspace-farmer https://github.com/subspace/subspace/releases/download/gemini-1b-2022-jun-10/subspace-farmer-ubuntu-x86_64-gemini-1b-2022-jun-10
 chmod +x subspace-node subspace-farmer
 mv subspace-node subspace-farmer /usr/local/bin/
 
