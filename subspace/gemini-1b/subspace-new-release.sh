86fafbabb34a3d3967ba5b8edb285588da3cc870
CBzeek <60964131+CBzeek@users.noreply.github.com>
2022-06-22 09:25:26 +0300

    Update subspace-new-release.sh

--- subspace/gemini-1b/subspace-new-release.sh
+++ subspace/gemini-1b/subspace-new-release.sh
@@ -5,18 +5,24 @@ echo '### Stopping subspace service'
 echo ''
 sudo systemctl stop subspaced subspaced-farmer 
 
-echo '### Restart systemd'
-echo ''
-sudo systemctl restart systemd-journald
-sudo systemctl daemon-reload
-sudo systemctl enable subspaced subspaced-farmer
-
 echo '### Download new Subspace release'
 echo ''
 wget -O subspace-node https://github.com/subspace/subspace/releases/download/gemini-1b-2022-jun-13/subspace-node-ubuntu-x86_64-gemini-1b-2022-jun-13
 wget -O subspace-farmer https://github.com/subspace/subspace/releases/download/gemini-1b-2022-jun-13/subspace-farmer-ubuntu-x86_64-gemini-1b-2022-jun-13
 chmod +x subspace-node subspace-farmer
 mv subspace-node subspace-farmer /usr/local/bin/
+
+echo '### Install additional packages'
+echo ''
+sudo apt install -y ocl-icd-opencl-dev
+sudo apt install -y intel-opencl-icd
+sudo apt install -y libgomp1
+
+echo '### Restart systemd'
+echo ''
+sudo systemctl restart systemd-journald
+sudo systemctl daemon-reload
+sudo systemctl enable subspaced subspaced-farmer
 
 echo '### Starting subspace node service'
 echo ''
