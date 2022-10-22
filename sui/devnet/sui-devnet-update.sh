92b6c2be09e3a07623f55c14bc635b288b7349d1
CBzeek <60964131+CBzeek@users.noreply.github.com>
2022-10-22 18:24:33 +0300

    Create sui-devnet-update.sh

---
+++ sui/devnet/sui-devnet-update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd $HOME
+
+echo '### Stopping subspace service'
+echo ''
+
+
+echo '### Restart systemd'
+echo ''
+
+echo '### Erasing plots'
+echo ''
+
+echo '### Starting subspace node service'
+echo ''
+
+echo '### Starting subspace farmer service'
+echo ''
