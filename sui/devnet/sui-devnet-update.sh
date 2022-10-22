238a135d5bd07c602d4c14965825f4db94490b79
CBzeek <60964131+CBzeek@users.noreply.github.com>
2022-10-22 18:51:43 +0300

    Update sui-devnet-update.sh

--- sui/devnet/sui-devnet-update.sh
+++ sui/devnet/sui-devnet-update.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 cd $HOME
 
-echo '### Stopping subspace service'
+echo '### Stopping sui service'
 echo ''
+systemctl stop suid
 
-
-echo '### Restart systemd'
 echo ''
-
-echo '### Erasing plots'
+echo '### Erase data'
 echo ''
+rm -rf /var/sui/db/* /var/sui/genesis.blob $HOME/sui
 
-echo '### Starting subspace node service'
 echo ''
+echo '### Sui update'
+echo ''
+source $HOME/.cargo/env
+cd $HOME
+git clone https://github.com/MystenLabs/sui.git
+cd sui
+git remote add upstream https://github.com/MystenLabs/sui
+git fetch upstream
+git checkout -B devnet --track upstream/devnet
+cargo build -p sui-node -p sui --release
+mv ~/sui/target/release/sui-node /usr/local/bin/
+mv ~/sui/target/release/sui /usr/local/bin/
+wget -O /var/sui/genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
 
-echo '### Starting subspace farmer service'
 echo ''
+echo '### Starting sui node service'
+echo ''
+systemctl restart suid
