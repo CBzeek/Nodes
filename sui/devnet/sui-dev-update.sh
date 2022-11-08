83e05a073da20838f89fc171fd41bd50e3b08597
CBzeek <60964131+CBzeek@users.noreply.github.com>
2022-11-08 21:56:28 +0300

    Update sui-dev-update.sh

--- sui/devnet/sui-dev-update.sh
+++ sui/devnet/sui-dev-update.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 cd $HOME
 
-echo '### Stopping sui service'
+PROJECT_NAME="sui"
+
+#Stop service
+echo ''
+echo -e "\e[1m\e[32m### Stopping $PROJECT_NAME service... \e[0m" && sleep 1
 echo ''
 systemctl stop suid
 
+#Erase data
 echo ''
-echo '### Erase data'
+echo -e "\e[1m\e[32m### Erasing $PROJECT_NAME data... \e[0m" && sleep 1
 echo ''
 rm -rf /var/sui/db/* /var/sui/genesis.blob $HOME/sui
+mkdir -p /var/sui/db
 
+#Update
 echo ''
-echo '### Sui update'
+echo -e "\e[1m\e[32m### Updating $PROJECT_NAME... \e[0m" && sleep 1
 echo ''
 source $HOME/.cargo/env
 cd $HOME
@@ -25,7 +32,8 @@ mv ~/sui/target/release/sui-node /usr/local/bin/
 mv ~/sui/target/release/sui /usr/local/bin/
 wget -O /var/sui/genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
 
+#Restart service 
 echo ''
-echo '### Starting sui node service'
+echo '### Starting $PROJECT_NAME service'
 echo ''
 systemctl restart suid
