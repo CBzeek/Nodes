e43b1385fe8938cc76934e388a9d6ba1ba7ae570
CBzeek <60964131+CBzeek@users.noreply.github.com>
2023-01-01 12:21:46 +0300

    Create server-prepare.sh

---
+++ !tools/server-prepare.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd $HOME
+
+PROJECT_NAME="New server"
+
+#Ubuntu update and upgrade
+echo '#############################################################'
+echo -e "\e[1m\e[32m### Updating and Upgrading $PROJECT_NAME... \e[0m" && sleep 1
+echo ''
+sudo apt update && sudo apt upgrade -y
+
+#Install software
+echo '#############################################################'
+echo -e "\e[1m\e[32m### Installing dependencies to $PROJECT_NAME... \e[0m" && sleep 1
+echo ''
+sudo apt install -y curl
+sudo apt install -y mc
