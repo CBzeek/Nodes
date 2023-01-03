0bcffe54ddcf6d3331be450a3bfce11422aac1db
CBzeek <60964131+CBzeek@users.noreply.github.com>
2023-01-03 23:07:48 +0300

    Update server-prepare.sh

--- !tools/server-prepare.sh
+++ !tools/server-prepare.sh
@@ -13,4 +13,4 @@ sudo apt update && sudo apt upgrade -y
 echo '#############################################################'
 echo -e "\e[1m\e[32m### Installing dependencies to $PROJECT_NAME... \e[0m" && sleep 1
 echo ''
-sudo apt install -y mc git jq screen lz4
+sudo apt install -y mc git jq screen lz4 htop
