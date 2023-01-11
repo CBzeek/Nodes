1643717f100cc8a068020aa4936f477754156b03
CBzeek <60964131+CBzeek@users.noreply.github.com>
2023-01-11 10:30:49 +0300

    Update lava-update.sh

--- lava/lava-testnet-1/lava-update.sh
+++ lava/lava-testnet-1/lava-update.sh
@@ -23,7 +23,23 @@ git checkout v0.4.3
 make install
 ${PROJECT_NAME} version
 
+sudo tee /etc/systemd/system/lavad.service > /dev/null << EOF
+[Unit]
+Description=Lava Network Node
+After=network-online.target
+[Service]
+User=$USER
+ExecStart=$(which lavad) start
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=10000
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
 echo ''
 echo -e "\e[1m\e[32m### Starting $PROJECT_NAME service... \e[0m" && sleep 1
 echo ''
-sudo systemctl restart ${PROJECT_NAME}
+sudo systemctl daemon-reload
+sudo systemctl start ${PROJECT_NAME}
