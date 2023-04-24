8c32de4c02fdbea861afdf1e1649c6a964cdbe8a
CBzeek <60964131+CBzeek@users.noreply.github.com>
2023-04-24 23:49:24 +0300

    Update nibiru.sh

--- nibiru/itn-1/nibiru.sh
+++ nibiru/itn-1/nibiru.sh
@@ -4,20 +4,20 @@ PROJECT_NAME="nibid"
 cd $HOME
 
 
-echo ''
+echo '###########################################################################################'
 echo -e "\e[1m\e[32m### Install dependencies... \e[0m" && sleep 1
 echo ''
 source <(wget -O- 'https://raw.githubusercontent.com/CBzeek/Nodes/main/!tools/server-prepare.sh')
 
 
-echo ''
+echo '###########################################################################################'
 echo -e "\e[1m\e[32m### Installing $PROJECT_NAME node... \e[0m" && sleep 1
 echo ''
 source <(wget -O- https://api.nodes.guru/nibiru.sh)
 source $HOME/.bash_profile
 
 
-echo ''
+echo '###########################################################################################'
 echo -e "\e[1m\e[32m### Creating $PROJECT_NAME wallet.. \e[0m" && sleep 1
 echo ''
 nibid keys add wallet
