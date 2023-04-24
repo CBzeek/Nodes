5edcea5a2d3c34cf5e5cdab38b9f209eb0322306
CBzeek <60964131+CBzeek@users.noreply.github.com>
2023-04-25 00:05:26 +0300

    Update nibiru-faucet.sh

--- nibiru/itn-1/nibiru-faucet.sh
+++ nibiru/itn-1/nibiru-faucet.sh
@@ -6,5 +6,6 @@ cd $HOME
 echo '###########################################################################################'
 echo -e "\e[1m\e[32m### Request tokens from $PROJECT_NAME faucet... \e[0m" && sleep 1
 echo ''
-echo $1 
-
+FAUCET_URL="https://faucet.itn-1.nibiru.fi/"
+ADDR=$1 # your address 
+curl -X POST -d '{"address": "'"$ADDR"'", "coins": ["11000000unibi","100000000unusd","100000000uusdt"]}' $FAUCET_URL
