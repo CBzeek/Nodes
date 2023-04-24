71bbb3f285a1135c2912f54ae15619454346b68a
CBzeek <60964131+CBzeek@users.noreply.github.com>
2023-04-24 23:48:44 +0300

    Update server-prepare.sh

--- !tools/server-prepare.sh
+++ !tools/server-prepare.sh
@@ -4,13 +4,13 @@ cd $HOME
 PROJECT_NAME="New server"
 
 #Ubuntu update and upgrade
-echo '#############################################################'
+echo '###########################################################################################'
 echo -e "\e[1m\e[32m### Updating and Upgrading $PROJECT_NAME... \e[0m" && sleep 1
 echo ''
 sudo apt update && sudo apt upgrade -y
 
 #Install software
-echo '#############################################################'
+echo '###########################################################################################'
 echo -e "\e[1m\e[32m### Installing dependencies to $PROJECT_NAME... \e[0m" && sleep 1
 echo ''
 sudo apt install -y curl mc git jq screen lz4 htop
