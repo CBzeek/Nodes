4f44fedb127d02837477f7054b5c1a77c2e94c0c
CBzeek <60964131+CBzeek@users.noreply.github.com>
2023-04-24 23:58:35 +0300

    Create nibiru-faucet.sh

---
+++ nibiru/itn-1/nibiru-faucet.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+PROJECT_NAME="nibid"
+
+cd $HOME
+
+echo '###########################################################################################'
+echo -e "\e[1m\e[32m### Request tokens from $PROJECT_NAME faucet... \e[0m" && sleep 1
+echo ''
+echo $1 
+
