af54a51a8cfe077aeca21252503e091d041e7b57
CBzeek <60964131+CBzeek@users.noreply.github.com>
2023-04-24 23:49:10 +0300

    Update nibiru-snapshot.sh

--- nibiru/itn-1/nibiru-snapshot.sh
+++ nibiru/itn-1/nibiru-snapshot.sh
@@ -3,19 +3,20 @@ PROJECT_NAME="nibid"
 
 cd $HOME
 
-echo ''
+
+echo '###########################################################################################'
 echo -e "\e[1m\e[32m### Install dependencies... \e[0m" && sleep 1
 echo ''
 source <(wget -O- 'https://raw.githubusercontent.com/CBzeek/Nodes/main/!tools/server-prepare.sh')
 
 
-echo ''
+echo '###########################################################################################'
 echo -e "\e[1m\e[32m### Stopping $PROJECT_NAME service... \e[0m" && sleep 1
 echo ''
 sudo systemctl stop nibid
 
 
-echo ''
+echo '###########################################################################################'
 echo -e "\e[1m\e[32m### Updating $PROJECT_NAME snapshot... \e[0m" && sleep 1
 echo ''
 cp $HOME/.nibid/data/priv_validator_state.json $HOME/.nibid/priv_validator_state.json.backup
@@ -25,7 +26,7 @@ curl -L https://snapshots.kjnodes.com/nibiru-testnet/snapshot_latest.tar.lz4 | t
 mv $HOME/.nibid/priv_validator_state.json.backup $HOME/.nibid/data/priv_validator_state.json
 
 
-echo ''
+echo '###########################################################################################'
 echo -e "\e[1m\e[32m### Starting $PROJECT_NAME service... \e[0m" && sleep 1
 echo ''
 sudo systemctl start nibid
