ec09ec15582277ab4193c226e69466a7cd045c06
CBzeek <60964131+CBzeek@users.noreply.github.com>
2023-04-27 14:25:38 +0300

    Update massa-boostrap-ipv4.sh

--- massa/test-21/massa-boostrap-ipv4.sh
+++ massa/test-21/massa-boostrap-ipv4.sh
@@ -4,13 +4,6 @@ PROJECT_NAME="massa"
 cd $HOME
 
 echo '###########################################################################################'
-echo -e "\e[1m\e[32m### Stoping $PROJECT_NAME node... \e[0m" && sleep 1
-echo ''
-sudo systemctl stop massad
-cp $HOME/massa/massa-node/config/config.toml $HOME/massa/massa-node/config/config.toml.backup
-
-
-echo '###########################################################################################'
 echo -e "\e[1m\e[32m### Updating $PROJECT_NAME node boostrap options... \e[0m" && sleep 1
 echo ''
 if grep -Fxq 'bootstrap_protocol' $HOME/massa/massa-node/config/config.toml
@@ -18,6 +11,12 @@ then
     echo "bootstrap_protocol parameter found, no need to update..."
     echo ''
 else
+    echo '###########################################################################################'
+    echo -e "\e[1m\e[32m### Stoping $PROJECT_NAME node... \e[0m" && sleep 1
+    echo ''
+    sudo systemctl stop massad
+    cp $HOME/massa/massa-node/config/config.toml $HOME/massa/massa-node/config/config.toml.backup
+    
     echo "bootstrap_protocol parameter not found, starting update..."
     echo ''
 
@@ -31,13 +30,14 @@ EOF
 
 echo "Config successfully updated..."
 echo ''
-fi
-
 
 echo '###########################################################################################'
 echo -e "\e[1m\e[32m### Starting $PROJECT_NAME node... \e[0m" && sleep 1
 echo ''
 sudo systemctl restart massad
+fi
+
+
 
 
 
