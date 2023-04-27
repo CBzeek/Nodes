c770a3cf3aa692239c77e89e28d436b155528535
CBzeek <60964131+CBzeek@users.noreply.github.com>
2023-04-27 14:05:01 +0300

    Create massa-boostrap-ipv4.sh

---
+++ massa/test-21/massa-boostrap-ipv4.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+PROJECT_NAME="massa"
+
+cd $HOME
+
+echo '###########################################################################################'
+echo -e "\e[1m\e[32m### Stoping $PROJECT_NAME node... \e[0m" && sleep 1
+echo ''
+#sudo systemctl stop massad
+cp $HOME/massa/massa-node/config/config.toml $HOME/massa/massa-node/config/config.toml.backup
+
+echo '###########################################################################################'
+echo -e "\e[1m\e[32m### Updating $PROJECT_NAME node boostrap options... \e[0m" && sleep 1
+echo ''
+if grep -Fxq 'bootstrap_protocol' $HOME/massa/massa-node/config/config.toml
+then
+    echo "bootstrap_protocol parameter found, no need to update."
+else
+    echo "bootstrap_protocol parameter not found, update needed."
+    echo "Starting update..."
+
+sudo tee $HOME/config.toml >> /dev/null <<EOF
+
+[bootstrap]
+# force the bootstrap protocol to use: "IPv4", "IPv6", or "Both". Defaults to using both protocols.
+bootstrap_protocol = "IPv4"
+
+EOF
+
+echo "Config successfully updated."
+fi
+
+
+echo '###########################################################################################'
+echo -e "\e[1m\e[32m### Starting $PROJECT_NAME node... \e[0m" && sleep 1
+echo ''
+#sudo systemctl restart massad
+
+
+
+
