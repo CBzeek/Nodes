68f8cf0269fa9f2a963916b0329d76cd99699827
CBzeek <60964131+CBzeek@users.noreply.github.com>
2023-05-20 15:26:36 +0300

    Update server-prepare.sh

--- !tools/server-prepare.sh
+++ !tools/server-prepare.sh
@@ -2,12 +2,14 @@
 cd $HOME
 
 #Ubuntu update and upgrade
+echo ''
 echo '###########################################################################################'
 echo -e "\e[1m\e[32m### Updating and Upgrading server... \e[0m" && sleep 1
 echo ''
 sudo apt update && sudo apt upgrade -y
 
 #Install software
+echo ''
 echo '###########################################################################################'
 echo -e "\e[1m\e[32m### Installing dependencies to server... \e[0m" && sleep 1
 echo ''
