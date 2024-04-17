d90907dc388a898e97c7f2eb36113919dba0449b
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-04-17 09:55:16 +0300

    Update 0g-snapshot.sh

--- 0g/v1.0.0-testnet/0g-snapshot.sh
+++ 0g/v1.0.0-testnet/0g-snapshot.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+PROJECT_NAME="0G"
 
+
+echo '###########################################################################################'
+echo -e "\e[1m\e[32m### Downloading $PROJECT_NAME node snapshot... \e[0m" && sleep 1
+echo ''
 wget https://rpc-zero-gravity-testnet.trusted-point.com/latest_snapshot.tar.lz4
 
+
+echo '###########################################################################################'
+echo -e "\e[1m\e[32m### Stopping $PROJECT_NAME node... \e[0m" && sleep 1
+echo ''
 sudo systemctl stop ogd
 
 cp $HOME/.evmosd/data/priv_validator_state.json $HOME/.evmosd/priv_validator_state.json.backup
@@ -12,5 +21,8 @@ lz4 -d -c ./latest_snapshot.tar.lz4 | tar -xf - -C $HOME/.evmosd
 
 mv $HOME/.evmosd/priv_validator_state.json.backup $HOME/.evmosd/data/priv_validator_state.json
 
+echo '###########################################################################################'
+echo -e "\e[1m\e[32m### Restarting $PROJECT_NAME node... \e[0m" && sleep 1
+echo ''
 sudo systemctl restart ogd && sudo journalctl -u ogd -f -o cat
 
