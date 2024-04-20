b019e2c96470ce068c4d5659ddfff7283f6a585d
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-04-20 09:57:22 +0300

    Update 0g-snapshot.sh

--- 0g/v1.0.0-testnet/0g-snapshot.sh
+++ 0g/v1.0.0-testnet/0g-snapshot.sh
@@ -5,7 +5,7 @@ PROJECT_NAME="0G"
 echo '###########################################################################################'
 echo -e "\e[1m\e[32m### Downloading $PROJECT_NAME node snapshot... \e[0m" && sleep 1
 echo ''
-wget https://rpc-zero-gravity-testnet.trusted-point.com/latest_snapshot.tar.lz4
+wget -O latest_snapshot.tar.lz4 https://rpc-zero-gravity-testnet.trusted-point.com/latest_snapshot.tar.lz4  s
 
 
 echo '###########################################################################################'
@@ -20,6 +20,7 @@ evmosd tendermint unsafe-reset-all --home $HOME/.evmosd --keep-addr-book
 lz4 -d -c ./latest_snapshot.tar.lz4 | tar -xf - -C $HOME/.evmosd
 
 mv $HOME/.evmosd/priv_validator_state.json.backup $HOME/.evmosd/data/priv_validator_state.json
+rm -f ./latest_snapshot.tar.lz4
 
 echo '###########################################################################################'
 echo -e "\e[1m\e[32m### Restarting $PROJECT_NAME node... \e[0m" && sleep 1
