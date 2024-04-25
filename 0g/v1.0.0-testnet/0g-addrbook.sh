0e477f516afce0cf1d88862d87f6b7d8eb181504
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-04-25 13:50:58 +0300

    Update 0g-addrbook.sh

--- 0g/v1.0.0-testnet/0g-addrbook.sh
+++ 0g/v1.0.0-testnet/0g-addrbook.sh
@@ -8,7 +8,7 @@ echo ''
 sudo systemctl stop ogd
 
 echo '###########################################################################################'
-echo -e "\e[1m\e[32m### Downloading $PROJECT_NAME node snapshot... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m### Downloading $PROJECT_NAME node address book... \e[0m" && sleep 1
 echo ''
 wget -O $HOME/.evmosd/config/addrbook.json https://rpc-zero-gravity-testnet.trusted-point.com/addrbook.json
 
