eb9abfcf0e8b10604a3975fe67022721d083003e
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-04-25 15:22:33 +0300

    Update 0g-snapshot.sh

--- 0g/v1.0.0-testnet/0g-snapshot.sh
+++ 0g/v1.0.0-testnet/0g-snapshot.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 PROJECT_NAME="0G"
 
-
-echo '###########################################################################################'
-echo -e "\e[1m\e[32m### Downloading $PROJECT_NAME node snapshot... \e[0m" && sleep 1
-echo ''
-wget -O latest_snapshot.tar.lz4 https://rpc-zero-gravity-testnet.trusted-point.com/latest_snapshot.tar.lz4
-
-
 echo '###########################################################################################'
 echo -e "\e[1m\e[32m### Stopping $PROJECT_NAME node... \e[0m" && sleep 1
 echo ''
 sudo systemctl stop ogd
 
+rm -rf $HOME/.evmosd/data/snapshots
 cp $HOME/.evmosd/data/priv_validator_state.json $HOME/.evmosd/priv_validator_state.json.backup
-
 evmosd tendermint unsafe-reset-all --home $HOME/.evmosd --keep-addr-book
 
+
+echo '###########################################################################################'
+echo -e "\e[1m\e[32m### Downloading $PROJECT_NAME node snapshot... \e[0m" && sleep 1
+echo ''
+wget -O latest_snapshot.tar.lz4 https://rpc-zero-gravity-testnet.trusted-point.com/latest_snapshot.tar.lz4
 lz4 -d -c ./latest_snapshot.tar.lz4 | tar -xf - -C $HOME/.evmosd
 
 mv $HOME/.evmosd/priv_validator_state.json.backup $HOME/.evmosd/data/priv_validator_state.json
