d42a151252ab215acd13031979dc3abbb42d68b8
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-05-12 13:10:40 +0300

    Create 0g.sh

---
+++ 0g/zgtendermint_16600-1/0g.sh
@@ -0,0 +1,171 @@
+#!/bin/bash
+PROJECT_NAME="0G"
+
+if [ ! $MONIKER ]; then
+    echo ""
+    echo -e "\e[1m\e[32m###########################################################################################"
+    echo -e "\e[1m\e[32m### Setting $PROJECT_NAME node moniker... \e[0m" && sleep 1
+    echo ""
+    read -p "Enter node moniker: " MONIKER
+    echo 'export MONIKER='\"${MONIKER}\" >> ~/.bash_profile
+fi
+#read -p "Enter node moniker: " MONIKER
+#echo 'export MONIKER='\"${MONIKER}\" >> ~/.bash_profile
+echo 'export CHAIN_ID="zgtendermint_16600-1"' >> ~/.bash_profile
+echo 'export WALLET_NAME="wallet"' >> ~/.bash_profile
+echo 'export RPC_PORT="26657"' >> ~/.bash_profile
+source $HOME/.bash_profile
+
+
+echo ""
+echo -e "\e[1m\e[32m###########################################################################################"
+echo -e "\e[1m\e[32m### Installing dependencies... \e[0m" && sleep 1
+echo ""
+#update
+sudo apt update && \
+sudo apt install curl git jq build-essential gcc unzip wget lz4 -y
+
+#install go
+cd $HOME && \
+ver="1.21.3" && \
+wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz" && \
+sudo rm -rf /usr/local/go && \
+sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz" && \
+rm "go$ver.linux-amd64.tar.gz" && \
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile && \
+source ~/.bash_profile && \
+go version
+
+
+echo ""
+echo -e "\e[1m\e[32m###########################################################################################"
+echo -e "\e[1m\e[32m### Installing $PROJECT_NAME node... \e[0m" && sleep 1
+echo ""
+#install binary 
+git clone -b v0.1.0 https://github.com/0glabs/0g-chain.git
+./0g-chain/networks/testnet/install.sh
+source .profile
+
+#init node
+cd $HOME
+0gchaind config chain-id $CHAIN_ID
+0gchaind init $MONIKER --chain-id $CHAIN_ID
+0gchaind config node tcp://localhost:$RPC_PORT
+0gchaind config keyring-backend os
+
+#get genesis
+wget -O $HOME/.0gchain/config https://github.com/0glabs/0g-chain/releases/download/v0.1.0/genesis.json
+
+#get addrbook
+wget -O $HOME/.0gchain/config https://snapshots.liveraven.net/snapshots/testnet/zero-gravity/addrbook.json
+#curl -Ls https://snapshots.liveraven.net/snapshots/testnet/zero-gravity/addrbook.json > $HOME/.0gchain/config/addrbook.json
+
+
+#peers, seeds
+PEERS="" && \
+SEEDS="c4d619f6088cb0b24b4ab43a0510bf9251ab5d7f@54.241.167.190:26656,44d11d4ba92a01b520923f51632d2450984d5886@54.176.175.48:26656,f2693dd86766b5bf8fd6ab87e2e970d564d20aff@54.193.250.204:26656,f878d40c538c8c23653a5b70f615f8dccec6fb9f@54.215.187.94:26656" && \
+sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.0gchain/config/config.toml
+
+#config
+EXTERNAL_IP=$(wget -qO- eth0.me) \
+PROXY_APP_PORT=26658 \
+P2P_PORT=26656 \
+PPROF_PORT=6060 \
+API_PORT=1317 \
+GRPC_PORT=9090 \
+GRPC_WEB_PORT=9091
+
+
+#set port
+sed -i \
+    -e "s/\(proxy_app = \"tcp:\/\/\)\([^:]*\):\([0-9]*\).*/\1\2:$PROXY_APP_PORT\"/" \
+    -e "s/\(laddr = \"tcp:\/\/\)\([^:]*\):\([0-9]*\).*/\1\2:$RPC_PORT\"/" \
+    -e "s/\(pprof_laddr = \"\)\([^:]*\):\([0-9]*\).*/\1localhost:$PPROF_PORT\"/" \
+    -e "/\[p2p\]/,/^\[/{s/\(laddr = \"tcp:\/\/\)\([^:]*\):\([0-9]*\).*/\1\2:$P2P_PORT\"/}" \
+    -e "/\[p2p\]/,/^\[/{s/\(external_address = \"\)\([^:]*\):\([0-9]*\).*/\1${EXTERNAL_IP}:$P2P_PORT\"/; t; s/\(external_address = \"\).*/\1${EXTERNAL_IP}:$P2P_PORT\"/}" \
+    $HOME/.0gchain/config/config.toml
+
+
+sed -i \
+    -e "/\[api\]/,/^\[/{s/\(address = \"tcp:\/\/\)\([^:]*\):\([0-9]*\)\(\".*\)/\1\2:$API_PORT\4/}" \
+    -e "/\[grpc\]/,/^\[/{s/\(address = \"\)\([^:]*\):\([0-9]*\)\(\".*\)/\1\2:$GRPC_PORT\4/}" \
+    -e "/\[grpc-web\]/,/^\[/{s/\(address = \"\)\([^:]*\):\([0-9]*\)\(\".*\)/\1\2:$GRPC_WEB_PORT\4/}" $HOME/.0gchain/config/app.toml
+
+#set pruning
+sed -i.bak -e "s/^pruning *=.*/pruning = \"custom\"/" $HOME/.0gchain/config/app.toml
+sed -i.bak -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"100\"/" $HOME/.0gchain/config/app.toml
+sed -i.bak -e "s/^pruning-interval *=.*/pruning-interval = \"10\"/" $HOME/.0gchain/config/app.toml
+
+#set gas
+sed -i "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0ua0gi\"/" $HOME/.0gchain/config/app.toml
+
+sed -i "s/^indexer *=.*/indexer = \"kv\"/" $HOME/.0gchain/config/config.toml
+
+
+echo ""
+echo -e "\e[1m\e[32m###########################################################################################"
+echo -e "\e[1m\e[32m### Downloading $PROJECT_NAME node snapshot... \e[0m" && sleep 1
+echo ""
+#shapshot
+cp $HOME/.0gchain/data/priv_validator_state.json $HOME/.0gchain/priv_validator_state.json.backup
+rm -rf $HOME/.0gchain/data
+curl -L http://snapshots.liveraven.net/snapshots/testnet/zero-gravity/zgtendermint_16600-1_latest.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.0gchain
+mv $HOME/.0gchain/priv_validator_state.json.backup $HOME/.0gchain/data/priv_validator_state.json
+
+
+
+echo ""
+echo -e "\e[1m\e[32m###########################################################################################"
+echo -e "\e[1m\e[32m### Setting $PROJECT_NAME node service... \e[0m" && sleep 1
+echo ""
+#service file
+sudo tee /etc/systemd/system/ogd.service > /dev/null <<EOF
+[Unit]
+Description=OG Node
+After=network.target
+[Service]
+User=$USER
+Type=simple
+ExecStart=$(which 0gchaind) start --home $HOME/.0gchain
+Restart=10
+LimitNOFILE=65535
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
+echo ""
+echo -e "\e[1m\e[32m###########################################################################################"
+echo -e "\e[1m\e[32m### Starting $PROJECT_NAME node... \e[0m" && sleep 1
+echo ""
+#start node
+sudo systemctl daemon-reload && \
+sudo systemctl enable ogd && \
+sudo systemctl restart ogd && sudo journalctl -u ogd -f -o cat
+
+
+echo ""
+echo -e "\e[1m\e[32m###########################################################################################"
+echo -e "\e[1m\e[32m### Creating $PROJECT_NAME node comsos wallet... \e[0m" && sleep 1
+echo ""
+echo "Select option:"
+echo "1 - Create a new wallet"
+echo "2 - Import an existing wallet"
+read -p "Enter option: " OPTION
+case $OPTION in
+    2)  #Import wallet
+        0gchaind keys add --recover $WALLET_NAME --eth
+        ;;
+    *)  #Create wallet
+        0gchaind keys add $WALLET_NAME --eth
+        ;;
+esac
+
+
+echo ""
+echo -e "\e[1m\e[32m###########################################################################################"
+echo -e "\e[1m\e[32m### Getting $PROJECT_NAME node EVM address... \e[0m" && sleep 1
+echo ""
+#get EVM privatekey
+0gchaind keys unsafe-export-eth-key $WALLET_NAME
+echo ""
