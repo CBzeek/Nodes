a1e2755ff5d3fdda9941c7bf79009dee49390ca8
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-05-12 13:37:16 +0300

    Create 0g-addrbook.sh

---
+++ 0g/zgtendermint_16600-1/0g-addrbook.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+PROJECT_NAME="0G"
+
+
+echo '###########################################################################################'
+echo -e "\e[1m\e[32m### Stopping $PROJECT_NAME node... \e[0m" && sleep 1
+echo ''
+sudo systemctl stop ogd
+
+echo '###########################################################################################'
+echo -e "\e[1m\e[32m### Downloading $PROJECT_NAME node address book... \e[0m" && sleep 1
+echo ''
+wget -O $HOME/.0gchain/config https://snapshots.liveraven.net/snapshots/testnet/zero-gravity/addrbook.json
+
+echo '###########################################################################################'
+echo -e "\e[1m\e[32m### Restarting $PROJECT_NAME node... \e[0m" && sleep 1
+echo ''
+sudo systemctl restart ogd
