114915a7c6038d3df5712ad6bb9e39cc222dcc6e
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-05-12 13:40:45 +0300

    Update 0g-addrbook.sh

--- 0g/zgtendermint_16600-1/0g-addrbook.sh
+++ 0g/zgtendermint_16600-1/0g-addrbook.sh
@@ -10,7 +10,7 @@ sudo systemctl stop ogd
 echo '###########################################################################################'
 echo -e "\e[1m\e[32m### Downloading $PROJECT_NAME node address book... \e[0m" && sleep 1
 echo ''
-wget -O $HOME/.0gchain/config https://snapshots.liveraven.net/snapshots/testnet/zero-gravity/addrbook.json
+wget -O $HOME/.0gchain/config/addrbook.json https://snapshots.liveraven.net/snapshots/testnet/zero-gravity/addrbook.json
 
 echo '###########################################################################################'
 echo -e "\e[1m\e[32m### Restarting $PROJECT_NAME node... \e[0m" && sleep 1
