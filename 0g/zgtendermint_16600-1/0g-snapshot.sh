4c4dddb38914cd1db0241051dea2789799791168
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-05-16 22:39:39 +0300

    Create 0g-snapshot.sh

---
+++ 0g/zgtendermint_16600-1/0g-snapshot.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+PROJECT_NAME="0G"
+
+echo '###########################################################################################'
+echo -e "\e[1m\e[32m### Stopping $PROJECT_NAME node... \e[0m" && sleep 1
+echo ''
+sudo systemctl stop ogd
+
+echo '###########################################################################################'
+echo -e "\e[1m\e[32m### Downloading $PROJECT_NAME node snapshot... \e[0m" && sleep 1
+echo ''
+cp $HOME/.0gchain/data/priv_validator_state.json $HOME/.0gchain/priv_validator_state.json.backup
+0gchaind tendermint unsafe-reset-all --home $HOME/.0gchain --keep-addr-book
+wget -O latest_snapshot.tar.lz4 http://snapshots.liveraven.net/snapshots/testnet/zero-gravity/zgtendermint_16600-1_latest.tar.lz4
+#wget -O latest_snapshot.tar.lz4 https://snapshots-testnet.nodejumper.io/0g-testnet/0g-testnet_latest.tar.lz4
+#wget -O latest_snapshot.tar.lz4 https://rpc-zero-gravity-testnet.trusted-point.com/latest_snapshot.tar.lz4
+lz4 -d -c ./latest_snapshot.tar.lz4 | tar -xf - -C $HOME/.0gchain
+mv $HOME/.0gchain/priv_validator_state.json.backup $HOME/.0gchain/data/priv_validator_state.json
+rm -f ./latest_snapshot.tar.lz4
+
+#rm -rf $HOME/.evmosd/data/snapshots
+#cp $HOME/.evmosd/data/priv_validator_state.json $HOME/.evmosd/priv_validator_state.json.backup
+#evmosd tendermint unsafe-reset-all --home $HOME/.evmosd --keep-addr-book
+#wget -O latest_snapshot.tar.lz4 https://rpc-zero-gravity-testnet.trusted-point.com/latest_snapshot.tar.lz4
+#lz4 -d -c ./latest_snapshot.tar.lz4 | tar -xf - -C $HOME/.evmosd
+#mv $HOME/.evmosd/priv_validator_state.json.backup $HOME/.evmosd/data/priv_validator_state.json
+#rm -f ./latest_snapshot.tar.lz4
+
+echo '###########################################################################################'
+echo -e "\e[1m\e[32m### Restarting $PROJECT_NAME node... \e[0m" && sleep 1
+echo ''
+sudo systemctl restart ogd
+
