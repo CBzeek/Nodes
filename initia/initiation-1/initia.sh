80f952a5af198c3ffd1de1a0f1f4e6e822257251
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-05-23 23:32:03 +0300

    Update initia.sh

--- initia/initiation-1/initia.sh
+++ initia/initiation-1/initia.sh
@@ -80,7 +80,8 @@ echo ""
 cp $HOME/.initia/data/priv_validator_state.json $HOME/.initia/priv_validator_state.json.backup
 initiad tendermint unsafe-reset-all --home $HOME/.initia --keep-addr-book
 #rm -rf $HOME/.initia/data
-curl -L https://snapshots.kzvn.xyz/initia/initiation-1_latest.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.initia
+#curl -L https://snapshots.kzvn.xyz/initia/initiation-1_latest.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.initia
+curl -L https://snapshots-testnet.nodejumper.io/initia-testnet/initia-testnet_latest.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.initia
 mv $HOME/.initia/priv_validator_state.json.backup $HOME/.initia/data/priv_validator_state.json
 
 
