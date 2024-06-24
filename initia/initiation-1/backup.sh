b37e3496f378eb825a477a0dc141d6310b439b6f
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-06-24 18:02:16 +0300

    Update backup.sh

--- initia/initiation-1/backup.sh
+++ initia/initiation-1/backup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 PROJECT_NAME="Initia"
 PROJECT_FOLDER=".initia"
-BACKUP_DIR=$HOME/backup_$(date +%F--%R)
+BACKUP_DIR=$HOME/backup_$(date +%F--%R)/initia
 
 echo -e "\e[1m\e[32m###########################################################################################"
 echo -e "\e[1m\e[32m### Backup $PROJECT_NAME node configuration files... \e[0m" && sleep 1
 echo ''
 
 cd $HOME
-mkdir -p $BACKUP_DIR/$PROJECT_FOLDER/config/
-cp -r $HOME/$PROJECT_FOLDER/config/priv_validator_key.json $BACKUP_DIR/$PROJECT_NAME/config/
-rsync -av --exclude='data' --exclude='config' --exclude='home' --exclude='state.db' $HOME/$PROJECT_FOLDER/ $BACKUP_DIR/$PROJECT_NAME/
+mkdir -p $BACKUP_DIR/config/
+cp -r $HOME/$PROJECT_FOLDER/config/priv_validator_key.json $BACKUP_DIR/config/
+rsync -av --exclude='data' --exclude='config' --exclude='home' --exclude='state.db' $HOME/$PROJECT_FOLDER/ $BACKUP_DIR
