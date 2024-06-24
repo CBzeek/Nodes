ad943aa9ae0d60673307838f27142f70ae9a1996
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-06-24 18:04:30 +0300

    Update 0g-backup.sh

--- 0g/zgtendermint_16600-1/0g-backup.sh
+++ 0g/zgtendermint_16600-1/0g-backup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 PROJECT_NAME="0G"
 PROJECT_FOLDER=".0gchain"
-BACKUP_DIR=$HOME/backup_$(date +%F--%R)
+BACKUP_DIR=$HOME/backup_$(date +%F--%R)/0G/
 
 echo -e "\e[1m\e[32m###########################################################################################"
 echo -e "\e[1m\e[32m### Backup $PROJECT_NAME node configuration files... \e[0m" && sleep 1
 echo ''
 
 cd $HOME
-mkdir -p $BACKUP_DIR/$PROJECT_FOLDER/config/
-cp -r $HOME/$PROJECT_FOLDER/config/priv_validator_key.json $BACKUP_DIR/$PROJECT_NAME/config/
-rsync -av --exclude='data' --exclude='config' $HOME/$PROJECT_FOLDER/ $BACKUP_DIR/$PROJECT_NAME/
+mkdir -p $BACKUP_DIR/config/
+cp -r $HOME/$PROJECT_FOLDER/config/priv_validator_key.json $BACKUP_DIR/config/
+rsync -av --exclude='data' --exclude='config' $HOME/$PROJECT_FOLDER/ $BACKUP_DIR
 
 
