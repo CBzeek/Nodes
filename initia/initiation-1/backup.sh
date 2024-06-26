aeab469733b96cf89772abb3a677602099dce421
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-06-26 11:14:10 +0300

    Update backup.sh

--- initia/initiation-1/backup.sh
+++ initia/initiation-1/backup.sh
@@ -3,10 +3,11 @@ PROJECT_NAME="Initia"
 PROJECT_FOLDER=".initia"
 BACKUP_DIR=$HOME/backup_$(date +%F--%H-%M)/initia/
 
+sudo apt install rsync -y
+
 echo -e "\e[1m\e[32m###########################################################################################"
 echo -e "\e[1m\e[32m### Backup $PROJECT_NAME node configuration files... \e[0m" && sleep 1
 echo ''
-
 cd $HOME
 mkdir -p $BACKUP_DIR/config/
 cp -r $HOME/$PROJECT_FOLDER/config/priv_validator_key.json $BACKUP_DIR/config/
