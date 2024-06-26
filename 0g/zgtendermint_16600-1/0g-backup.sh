18bd112dfbb5d4036e1e42a54d0a81f5aaecb5ae
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-06-26 11:13:08 +0300

    Update 0g-backup.sh

--- 0g/zgtendermint_16600-1/0g-backup.sh
+++ 0g/zgtendermint_16600-1/0g-backup.sh
@@ -7,6 +7,8 @@ echo -e "\e[1m\e[32m############################################################
 echo -e "\e[1m\e[32m### Backup $PROJECT_NAME node configuration files... \e[0m" && sleep 1
 echo ''
 
+sudo apt install rsync -y
+
 cd $HOME
 mkdir -p $BACKUP_DIR/config/
 cp -r $HOME/$PROJECT_FOLDER/config/priv_validator_key.json $BACKUP_DIR/config/
