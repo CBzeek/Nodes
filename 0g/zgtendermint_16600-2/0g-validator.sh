e0eede6844882e63335a4ad9c0e503edf3c631d7
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-06-29 01:58:12 +0300

    Create 0g-validator.sh

---
+++ 0g/zgtendermint_16600-2/0g-validator.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+PROJECT_NAME="0G"
+
+echo ''
+echo -e "\e[1m\e[32m###########################################################################################"
+echo -e "\e[1m\e[32m### Creating $PROJECT_NAME node validator... \e[0m" && sleep 1
+echo ''
+#create validator
+0gchaind tx staking create-validator \
+  --amount=100000ua0gi \
+  --pubkey=$(0gchaind tendermint show-validator) \
+  --moniker="$MONIKER" \
+  --chain-id=$CHAIN_ID \
+  --commission-rate="0.10" \
+  --commission-max-rate="0.20" \
+  --commission-max-change-rate="0.01" \
+  --min-self-delegation="1" \
+  --from=$WALLET_NAME \
+  --identity="" \
+  --website="" \
+  --details="" \
+  --gas=auto \
+  --gas-adjustment=1.4 \
+  --yes
+  
