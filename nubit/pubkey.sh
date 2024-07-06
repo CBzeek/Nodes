3cd911a44f2d40920340c3ec13ad119b29cf48dd
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-07-06 17:16:12 +0300

    Update pubkey.sh

--- nubit/pubkey.sh
+++ nubit/pubkey.sh
@@ -6,4 +6,4 @@ echo -e "\e[1m\e[32m############################################################
 echo -e "\e[1m\e[32m### Showing $PROJECT_NAME node pubkey... \e[0m" && sleep 1
 echo ""
 $HOME/nubit-node/bin/nkey list --keyring-dir $HOME/.nubit-light-nubit-alphatestnet-1/keys/ --output json | jq -r '.[].pubkey' | jq -r '.key'
-
+echo ""
