806486d480c280c7a613f7195b710b3d1df83b45
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-07-17 11:11:35 +0300

    Create validator.sh

---
+++ artela/artela_11822-1/validator.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+PROJECT_NAME="Artela"
+PROJECT_BIN="artelad"
+
+echo ''
+echo -e "\e[1m\e[32m###########################################################################################"
+echo -e "\e[1m\e[32m### Creating $PROJECT_NAME node validator... \e[0m" && sleep 1
+echo ''
+#create validator
+artelad tx staking create-validator \
+  --amount=1000000uart \
+  --pubkey=$(artelad tendermint show-validator) \
+  --moniker="$MONIKER" \
+  --chain-id=$CHAIN_ID \
+  --commission-rate=0.10 \
+  --commission-max-rate=0.20 \
+  --commission-max-change-rate=0.01 \
+  --min-self-delegation=1 \
+  --from=$WALLET_NAME \
+  --identity="" \
+  --website="" \
+  --details="" \
+  --gas=auto \
+  --gas-adjustment=1.5 \
+  --yes
+
+
+#  --gas-prices=0.1uart \
