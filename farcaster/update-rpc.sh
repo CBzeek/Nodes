477be2157fbdd968bb9a4732f7ba676f2efb02b5
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-07-17 23:00:55 +0300

    Update update-rpc.sh

--- farcaster/update-rpc.sh
+++ farcaster/update-rpc.sh
@@ -11,5 +11,5 @@ read -p "Enter RPC for Optimism Mainnet: " RPC_OPT
 echo $RPC_ETH
 echo $RPC_OPT
 
-sed -i.bak -e "s|ETH_MAINNET_RPC_URL=.*|ETH_MAINNET_RPC_URL=$RPC_ETH|" $HOME/hubble/env
-sed -i.bak -e "s|OPTIMISM_L2_RPC_URL=.*|OPTIMISM_L2_RPC_URL=$RPC_ETH|" $HOME/hubble/env
+sed -i.bak -e "s|ETH_MAINNET_RPC_URL=.*|ETH_MAINNET_RPC_URL=$RPC_ETH|" $HOME/hubble/.env
+sed -i.bak -e "s|OPTIMISM_L2_RPC_URL=.*|OPTIMISM_L2_RPC_URL=$RPC_ETH|" $HOME/hubble/.env
