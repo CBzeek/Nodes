6fa23fef66f780846c0a2b606fd7a2b40b448556
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-08-03 00:12:17 +0300

    Update localhost.sh

--- nubit/localhost.sh
+++ nubit/localhost.sh
@@ -6,4 +6,6 @@ echo -e "\e[1m\e[32m############################################################
 echo -e "\e[1m\e[32m### Adding $PROJECT_NAME node localhost settings... \e[0m" && sleep 1
 echo ""
 # Add "localhost" to /etc/hosts
-sed -i 's/127.0.0.1 /127.0.0.1 localhost /' /etc/hosts
+#sed -i 's/127.0.0.1 /127.0.0.1 localhost /' /etc/hosts
+echo "127.0.0.1 localhost" >> /etc/hosts
+
