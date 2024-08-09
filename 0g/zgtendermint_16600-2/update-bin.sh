835e2e275edcf43934b39e6fb41458ab35a2e83d
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-08-10 01:26:27 +0300

    Update update-bin.sh

--- 0g/zgtendermint_16600-2/update-bin.sh
+++ 0g/zgtendermint_16600-2/update-bin.sh
@@ -7,7 +7,24 @@ echo -e "\e[1m\e[32m### Updateing $PROJECT_NAME node to version v0.3.0... \e[0m"
 echo ""
 # build new binary
 cd && rm -rf 0g-chain
-git clone -b v0.3.0 https://github.com/0glabs/0g-chain.git
+
+if [ -n "$1" ]
+then
+    if [ $1 = "v0.2.3" ]
+    then
+        git clone -b v0.2.3 https://github.com/0glabs/0g-chain.git
+    fi
+
+    if [ $1 = "v0.2.5" ]
+    then
+        git clone -b v0.2.5 https://github.com/0glabs/0g-chain.git
+    fi
+
+else
+    git clone -b v0.3.0 https://github.com/0glabs/0g-chain.git
+fi
+
+
 cd 0g-chain
 make install
 0gchaind version
