8d905b0f89214193305a8249ccd17de831e07c31
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-08-15 00:02:33 +0300

    Update update-bin.sh

--- 0g/zgtendermint_16600-2/update-bin.sh
+++ 0g/zgtendermint_16600-2/update-bin.sh
@@ -3,7 +3,7 @@ PROJECT_NAME="0G"
 
 echo ""
 echo -e "\e[1m\e[32m###########################################################################################"
-echo -e "\e[1m\e[32m### Updateing $PROJECT_NAME node to version v0.3.0... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m### Updateing $PROJECT_NAME node to version v0.3.1... \e[0m" && sleep 1
 echo ""
 # build new binary
 cd && rm -rf 0g-chain
@@ -21,7 +21,8 @@ then
     fi
 
 else
-    git clone -b v0.3.0 https://github.com/0glabs/0g-chain.git
+    git clone -b v0.3.1 https://github.com/0glabs/0g-chain.git
+#    git clone -b v0.3.0 https://github.com/0glabs/0g-chain.git
 fi
 
 
