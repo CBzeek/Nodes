4be0b5c727ecf1817d6dc036f0f0a68d861427e3
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-09-13 02:11:08 +0300

    Create install.sh

---
+++ elixir/testnet-3/install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+PROJECT_NAME="Elixir"
+
+echo ""
+echo -e "\e[1m\e[32m###########################################################################################"
+echo -e "\e[1m\e[32m### Installing dependencies... \e[0m" && sleep 1
+echo ""
+#update
+#sudo apt update
+#sudo apt install curl git jq build-essential lz4 unzip tar mc chrony htop ncdu nload screen -y 
+
+
+echo ""
+echo -e "\e[1m\e[32m###########################################################################################"
+echo -e "\e[1m\e[32m### Setting $PROJECT_NAME node params... \e[0m" && sleep 1
+echo ""
+read -p "Enter node moniker: " MONIKER
+read -p "Enter ETH address to receive/claim rewards: " REWARDS_ADDRESS
+read -p "Enter validator private key: " PRIVATE_KEY
+
+mkdir $HOME/.elixir && cd $HOME/.elixir
+
+sudo tee $HOME/.elixir/validator.env > /dev/null <<EOF
+ENV=testnet-3
+STRATEGY_EXECUTOR_DISPLAY_NAME=$MONIKER
+STRATEGY_EXECUTOR_BENEFICIARY=$REWARDS_ADDRESS
+SIGNER_PRIVATE_KEY=$PRIVATE_KEY
+EOF
+
+
