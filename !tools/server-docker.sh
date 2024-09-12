59c3c12a8fe01f664eca65bcdf906211385bbbf9
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-09-13 01:42:39 +0300

    Update server-docker.sh

--- !tools/server-docker.sh
+++ !tools/server-docker.sh
@@ -4,7 +4,7 @@ cd $HOME
 #Install Docker
 echo ''
 echo -e "\e[1m\e[32m###########################################################################################"
-echo -e "\e[1m\e[32m### Install docker to server... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m### Install Docker to server... \e[0m" && sleep 1
 echo ''
 curl -fsSL https://get.docker.com -o get-docker.sh 
 sudo sh get-docker.sh
