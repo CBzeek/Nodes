460e002dd4a43fb7dbda0ecd202c520c19699289
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-09-13 02:46:40 +0300

    Update server-prepare.sh

--- !tools/server-prepare.sh
+++ !tools/server-prepare.sh
@@ -13,4 +13,4 @@ echo ''
 echo -e "\e[1m\e[32m###########################################################################################"
 echo -e "\e[1m\e[32m### Installing dependencies to server... \e[0m" && sleep 1
 echo ''
-sudo apt install -y curl mc git jq screen lz4 htop
+sudo apt install curl mc git jq screen lz4 htop -y
