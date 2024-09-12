d962bf11fb12657d3a5dcba40cb165b1b89b6689
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-09-13 01:40:52 +0300

    Create server-docker-remove.sh

---
+++ !tools/server-docker-remove.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd $HOME
+
+#Install Docker
+echo ''
+echo -e "\e[1m\e[32m###########################################################################################"
+echo -e "\e[1m\e[32m### Ununstall Docker from server... \e[0m" && sleep 1
+echo ''
+sudo apt-get purge docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras
+sudo rm -rf /var/lib/docker
+sudo rm -rf /var/lib/containerd
