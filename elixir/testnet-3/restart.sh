4a8c1ce32a39866b4233c38bbf07bd5e157c69db
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-09-14 13:51:15 +0300

    Update restart.sh

--- elixir/testnet-3/restart.sh
+++ elixir/testnet-3/restart.sh
@@ -3,8 +3,11 @@ PROJECT_NAME="Elixir"
 
 echo ""
 echo -e "\e[1m\e[32m###########################################################################################"
-echo -e "\e[1m\e[32m### Start $PROJECT_NAME node ... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m### Restart $PROJECT_NAME node ... \e[0m" && sleep 1
 echo ""
+docker kill elixir
+docker rm elixir
+
 docker run -d \
   --env-file $HOME/.elixir/validator.env \
   --name elixir \
