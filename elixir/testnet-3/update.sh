ac2e3db0426a1606976d8ff859dbe1739b162471
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-09-14 14:42:44 +0300

    Update update.sh

--- elixir/testnet-3/update.sh
+++ elixir/testnet-3/update.sh
@@ -25,11 +25,3 @@ docker run -d \
   --name elixir \
   -p 17690:17690 \
   elixirprotocol/validator:v3
-
-
-echo ""
-echo -e "\e[1m\e[32m###########################################################################################"
-echo -e "\e[1m\e[32m### Get $PROJECT_NAME node version... \e[0m" && sleep 1
-echo ""
-sleep 3
-curl -s 127.0.0.1:17690/metrics | jq | grep app_version
