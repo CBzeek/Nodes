7db0b80daa626a045294d83f6cf9c51e27c6dccb
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-10-03 19:28:02 +0300

    Update server-prepare.sh

--- !tools/server-prepare.sh
+++ !tools/server-prepare.sh
@@ -4,13 +4,13 @@ cd $HOME
 #Ubuntu update and upgrade
 echo ''
 echo -e "\e[1m\e[32m###########################################################################################"
-echo -e "\e[1m\e[32m### Updating and Upgrading server... \e[0m" && sleep 1
-echo ''
+echo -e "\e[1m\e[32m### Updating and Upgrading server... \e[0m"
+echo '' && sleep 1
 sudo apt update && sudo apt upgrade -y
 
 #Install software
 echo ''
 echo -e "\e[1m\e[32m###########################################################################################"
-echo -e "\e[1m\e[32m### Installing dependencies to server... \e[0m" && sleep 1
-echo ''
+echo -e "\e[1m\e[32m### Installing dependencies to server... \e[0m"
+echo '' && sleep 1
 sudo apt install curl mc git jq screen lz4 htop zip unzip -y
