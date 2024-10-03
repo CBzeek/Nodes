3564c329b10e88ad8a50959c5d701f49177b9e5b
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-10-03 21:55:18 +0300

    Update backup.sh

--- nubit/backup.sh
+++ nubit/backup.sh
@@ -3,11 +3,13 @@ PROJECT_NAME="Nubit"
 PROJECT_FOLDER=".nubit-light-nubit-alphatestnet-1"
 BACKUP_DIR=$HOME/backup_$(date +%F--%H-%M)/
 
+sudo apt install rsync zip -y
 
 echo ''
 echo -e "\e[1m\e[32m###########################################################################################"
 echo -e "\e[1m\e[32m### Backup $PROJECT_NAME node configuration files... \e[0m" && sleep 1
 echo ''
+
 cd $HOME
 mkdir -p $BACKUP_DIR/nubit-light-nubit-alphatestnet-1/
 mkdir -p $BACKUP_DIR/nubit-node/
