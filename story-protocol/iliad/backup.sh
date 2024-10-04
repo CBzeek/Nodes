fdea803bd9444534ed7aee7c8071415ea6826a32
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-10-04 10:24:56 +0300

    Update backup.sh

--- story-protocol/iliad/backup.sh
+++ story-protocol/iliad/backup.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 PROJECT_NAME="Story Protocol"
+PROJECT_FOLDER=".story"
+BACKUP_DIR=backup_$(date +%F--%H-%M-%S)
 
-echo ""
+sudo apt install rsync zip -y
+
+echo ''
 echo -e "\e[1m\e[32m###########################################################################################"
-echo -e "\e[1m\e[32m### Installing dependencies... \e[0m" && sleep 1
-echo ""
-sudo apt-get update -y && sudo apt upgrade -y && sudo apt-get install make build-essential pkg-config libssl-dev unzip tar lz4 gcc git jq -y
+echo -e "\e[1m\e[32m### Backup $PROJECT_NAME node configuration files... \e[0m" && sleep 1
+echo ''
 
+cd $HOME
+mkdir -p $HOME/$BACKUP_DIR/geth/iliad/geth/
+mkdir -p $HOME/$BACKUP_DIR/story/config/ $HOME/$BACKUP_DIR/story/data/
 
-echo ""
-echo -e "\e[1m\e[32m###########################################################################################"
-echo -e "\e[1m\e[32m### Installing $PROJECT_NAME node... \e[0m" && sleep 1
-echo ""
-source <(wget -qO- 'https://api.nodes.guru/story.sh')
+cp -r $HOME/$PROJECT_FOLDER//story/config/ $HOME/$BACKUP_DIR/story/
+rm -f $HOME/$BACKUP_DIR/story/config/addrbook.json
+rm -f $HOME/$BACKUP_DIR/story/config/genesis.json
+rm -f $HOME/$BACKUP_DIR/story/config/*.toml
+
+cp $HOME/$PROJECT_FOLDER//story/data/priv_validator_state.json $HOME/$BACKUP_DIR/story/data/
+
+cp $HOME/$BACKUP_DIR/geth/iliad/geth/jwtsecret $HOME/$BACKUP_DIR/geth/iliad/geth/
+cp $HOME/$BACKUP_DIR/geth/iliad/geth/nodekey $HOME/$BACKUP_DIR/geth/iliad/geth/
+
+zip -r $BACKUP_DIR.zip $HOME/$BACKUP_DIR
