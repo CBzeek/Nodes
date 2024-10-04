dcdf4f4fd41af69194c671b8eca9ba66aec4c48c
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-10-04 23:38:41 +0300

    Create backup.sh

---
+++ 0g/zgtendermint_16600-2/backup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+PROJECT_NAME="0G"
+PROJECT_DIR=".0gchain"
+BACKUP_DIR=backup_$(curl -s eth0.me)_$(date +%F--%H-%M-%S)
+
+sudo apt install rsync zip -y
+
+echo ''
+echo -e "\e[1m\e[32m###########################################################################################"
+echo -e "\e[1m\e[32m### Backup $PROJECT_NAME node configuration files... \e[0m" && sleep 1
+echo ''
+
+cd $HOME
+mkdir -p $BACKUP_DIR/0gchain/config/ $BACKUP_DIR/0gchain/data/
+cp $HOME/$PROJECT_DIR/data/*.json $BACKUP_DIR/0gchain/data/
+rsync -av --exclude='data' --exclude='log' $HOME/$PROJECT_DIR/ $BACKUP_DIR
+
+zip -r $BACKUP_DIR.zip $HOME/$BACKUP_DIR
