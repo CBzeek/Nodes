c71c45a45f3f158e374d7ae7f705cc77a3ffb5e5
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-10-04 10:27:51 +0300

    Update backup.sh

--- story-protocol/iliad/backup.sh
+++ story-protocol/iliad/backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PROJECT_NAME="Story Protocol"
-PROJECT_FOLDER=".story"
+PROJECT_DIR=".story"
 BACKUP_DIR=backup_$(date +%F--%H-%M-%S)
 
 sudo apt install rsync zip -y
@@ -14,14 +14,14 @@ cd $HOME
 mkdir -p $HOME/$BACKUP_DIR/geth/iliad/geth/
 mkdir -p $HOME/$BACKUP_DIR/story/config/ $HOME/$BACKUP_DIR/story/data/
 
-cp -r $HOME/$PROJECT_FOLDER//story/config/ $HOME/$BACKUP_DIR/story/
+cp -r $HOME/$PROJECT_DIR//story/config/ $HOME/$BACKUP_DIR/story/
 rm -f $HOME/$BACKUP_DIR/story/config/addrbook.json
 rm -f $HOME/$BACKUP_DIR/story/config/genesis.json
 rm -f $HOME/$BACKUP_DIR/story/config/*.toml
 
-cp $HOME/$PROJECT_FOLDER/story/data/priv_validator_state.json $HOME/$BACKUP_DIR/story/data/
+cp $HOME/$PROJECT_DIR/story/data/priv_validator_state.json $HOME/$BACKUP_DIR/story/data/
 
-cp $HOME/$PROJECT_FOLDER/geth/iliad/geth/jwtsecret $HOME/$BACKUP_DIR/geth/iliad/geth/
-cp $HOME/$PROJECT_FOLDER/geth/iliad/geth/nodekey $HOME/$BACKUP_DIR/geth/iliad/geth/
+cp $HOME/$PROJECT_DIR/geth/iliad/geth/jwtsecret $HOME/$BACKUP_DIR/geth/iliad/geth/
+cp $HOME/$PROJECT_DIR/geth/iliad/geth/nodekey $HOME/$BACKUP_DIR/geth/iliad/geth/
 
 zip -r $BACKUP_DIR.zip $HOME/$BACKUP_DIR
