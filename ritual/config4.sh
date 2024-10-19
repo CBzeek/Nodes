0e07f73be158d2e3a332708c77f81cfd15426dc6
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-10-19 11:07:05 +0300

    Update and rename config3.sh to config4.sh

--- ritual/config4.sh
+++ ritual/config4.sh
@@ -9,10 +9,10 @@ read -p "Enter validator private key: " PRIVATE_KEY
 
 # Change $HOME/infernet-container-starter/deploy/config.json
 sed -i '/rpc_url/c\        \"rpc_url\" : \"https://mainnet.base.org\",' $HOME/infernet-container-starter/deploy/config.json
-sed -i '/registry_address/c\        \"registry_address\" : \"0x3B1554f346DFe5c482Bb4BA31b880c1C18412170\",' $HOME/infernet-container-starter/deploy/config.json
-sed -i '/private_key/c\            \"private_key\" : \"\'$PRIVATE_KEY'",' $HOME/infernet-container-starter/deploy/config.json
-sed -i '/sleep/c\            \"sleep\" : \"2",' $HOME/infernet-container-starter/deploy/config.json
-sed -i '/batch_size/c\            \"batch_size\" : \"800",' $HOME/infernet-container-starter/deploy/config.json
+sed -i '/registry_address/c\        \"registry_address\": \"0x3B1554f346DFe5c482Bb4BA31b880c1C18412170\",' $HOME/infernet-container-starter/deploy/config.json
+sed -i '/private_key/c\            \"private_key\": \"\'$PRIVATE_KEY'",' $HOME/infernet-container-starter/deploy/config.json
+sed -i '/sleep/c\            \"sleep\": \"2",' $HOME/infernet-container-starter/deploy/config.json
+sed -i '/batch_size/c\            \"batch_size\": \"800",' $HOME/infernet-container-starter/deploy/config.json
 
 # copy config to $HOME/infernet-container-starter/projects/hello-world/container/config.jsonv
 cp $HOME/infernet-container-starter/deploy/config.json $HOME/infernet-container-starter/projects/hello-world/container/config.json
