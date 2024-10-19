4c7fac28b46cc10d4a29300e6bd2c7caa21e74c0
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-10-19 10:38:05 +0300

    Update and rename config2.sh to config3.sh

--- ritual/config3.sh
+++ ritual/config3.sh
@@ -10,7 +10,7 @@ read -p "Enter validator private key: " PRIVATE_KEY
 # Change $HOME/infernet-container-starter/deploy/config.json
 sed -i '/rpc_url/c\        \"rpc_url\" : \"https://mainnet.base.org\",' $HOME/infernet-container-starter/deploy/config.json
 sed -i '/registry_address/c\        \"registry_address\" : \"0x3B1554f346DFe5c482Bb4BA31b880c1C18412170\",' $HOME/infernet-container-starter/deploy/config.json
-sed -i '/private_key/c\            \"private_key\" : \"${PRIVATE_KEY}",' $HOME/infernet-container-starter/deploy/config.json
+sed -i '/private_key/c\            \"private_key\" : \"\'$PRIVATE_KEY'",' $HOME/infernet-container-starter/deploy/config.json
 sed -i '/sleep/c\            \"sleep\" : \"2",' $HOME/infernet-container-starter/deploy/config.json
 sed -i '/batch_size/c\            \"batch_size\" : \"800",' $HOME/infernet-container-starter/deploy/config.json
 
