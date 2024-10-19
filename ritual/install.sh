ec0ec3390eb02884133cdbd1e34894b101882cd5
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-10-19 10:55:28 +0300

    Update install2.sh

--- ritual/install.sh
+++ ritual/install.sh
@@ -6,6 +6,7 @@ echo -e "\e[1m\e[32m############################################################
 echo -e "\e[1m\e[32m### Installing $PROJECT_NAME node... \e[0m" && sleep 1
 echo ""
 # Clone Ritual's repository
+cd $HOME
 git clone https://github.com/ritual-net/infernet-container-starter
 cd infernet-container-starter
 sed -i 's/docker compose/docker-compose/' $HOME/infernet-container-starter/Makefile
