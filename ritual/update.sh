0affd9322f6921c53b882db47e4920d76a0397d9
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-10-31 10:28:35 +0300

    Create update.sh

---
+++ ritual/update.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+PROJECT_NAME="Ritual"
+
+echo ""
+echo -e "\e[1m\e[32m###########################################################################################"
+echo -e "\e[1m\e[32m### Update $PROJECT_NAME node... \e[0m" && sleep 1
+echo ""
+cd $HOME/infernet-container-starter
+
+# Change the node’s image to the latest version (today, this is 1.2.0 but be sure to check for the latest version).
+sed -i '/infernet-node\:1.2.0/c\    image\: ritualnetwork\/infernet-node\:1.4.0' $HOME/infernet-container-starter/deploy/docker-compose.yaml
+
+# Stop container
+docker-compose -f infernet-container-starter/deploy/docker-compose.yaml down
+
+# Restart docker container
+docker restart infernet-anvil
+docker restart hello-world
+docker restart infernet-node
+docker restart deploy-fluentbit-1
+docker restart deploy-redis-1
+
