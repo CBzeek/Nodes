86d80b0978125906ee676cbd3d37ac2e63d1b387
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-11-06 02:31:04 +0300

    Create update.sh

---
+++ elixir/mainnet/update.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+PROJECT_NAME="Elixir"
+
+source <(wget -qO- 'https://raw.githubusercontent.com/CBzeek/Nodes/main/elixir/testnet-3/backup.sh')
+
+echo ""
+echo -e "\e[1m\e[32m###########################################################################################"
+echo -e "\e[1m\e[32m### Delete $PROJECT_NAME node old docker image... \e[0m" && sleep 1
+echo ""
+docker kill elixir-mainnet
+docker rm elixir-mainnet
+
+echo ""
+echo -e "\e[1m\e[32m###########################################################################################"
+echo -e "\e[1m\e[32m### Get $PROJECT_NAME node docker image... \e[0m" && sleep 1
+echo ""
+docker pull elixirprotocol/validator --platform linux/amd64
+
+
+echo ""
+echo -e "\e[1m\e[32m###########################################################################################"
+echo -e "\e[1m\e[32m### Start $PROJECT_NAME node ... \e[0m" && sleep 1
+echo ""
+docker run -d \
+  --env-file $HOME/.elixir/validator-mainnet.env \
+  --platform linux/amd64 \
+  --name elixir-mainnet \
+  -p 17691:17691 \
+  elixirprotocol/validator
