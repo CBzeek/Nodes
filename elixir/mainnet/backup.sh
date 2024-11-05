3bd4c4227468ee40007c5505080787c8b4487736
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-11-06 00:34:59 +0300

    Create backup.sh

---
+++ elixir/mainnet/backup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+PROJECT_NAME="Elixir"
+PROJECT_DIR=".elixir"
+BACKUP_DIR=backup_$(curl -s eth0.me)_$(date +%F--%H-%M-%S)
+
+sudo apt install zip -y
+
+echo ''
+echo -e "\e[1m\e[32m###########################################################################################"
+echo -e "\e[1m\e[32m### Backup $PROJECT_NAME node configuration files... \e[0m" && sleep 1
+echo ''
+
+cd $HOME
+mkdir -p $BACKUP_DIR/elixir/ 
+cp $HOME/$PROJECT_DIR/* $BACKUP_DIR/elixir/
+
+zip -r $BACKUP_DIR.zip $HOME/$BACKUP_DIR
