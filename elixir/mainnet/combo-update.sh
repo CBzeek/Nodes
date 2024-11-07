191c171328bb40aee0741acaf73af8f5973e694a
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-11-07 23:53:00 +0300

    Create combo-update.sh

---
+++ elixir/mainnet/combo-update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source <(wget -qO- 'https://raw.githubusercontent.com/CBzeek/Nodes/refs/heads/main/elixir/mainnet/backup.sh')
+
+source <(wget -qO- 'https://raw.githubusercontent.com/CBzeek/Nodes/refs/heads/main/elixir/mainnet/update.sh')
+
+source <(wget -qO- 'https://raw.githubusercontent.com/CBzeek/Nodes/main/elixir/testnet-3/update.sh')
+
+
