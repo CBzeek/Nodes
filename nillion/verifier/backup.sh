621668881fab9137dc3012e5a62cf15d5d606504
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-11-08 22:56:15 +0300

    Update backup.sh

--- nillion/verifier/backup.sh
+++ nillion/verifier/backup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 PROJECT_NAME="Nillion"
-PROJECT_DIR="nillion/verifier"
+PROJECT_DIR="nillion"
+PROJECT_SUB_DIR="verifier"
 BACKUP_DIR=backup_$(curl -s eth0.me)_$(date +%F--%H-%M-%S)
 
 sudo apt install zip -y
@@ -11,7 +12,8 @@ echo -e "\e[1m\e[32m### Backup $PROJECT_NAME node configuration files... \e[0m" 
 echo ''
 
 cd $HOME
-mkdir -p $BACKUP_DIR/$PROJECT_DIR
-cp $HOME/$PROJECT_DIR/credentials.json $BACKUP_DIR/$PROJECT_DIR
-
-zip -r $BACKUP_DIR.zip $BACKUP_DIR
+mkdir -p $BACKUP_DIR/$PROJECT_DIR/$PROJECT_SUB_DIR
+cp $HOME/$PROJECT_DIR/$PROJECT_SUB_DIR/credentials.json $BACKUP_DIR/$PROJECT_DIR/$PROJECT_SUB_DIR
+cd $BACKUP_DIR
+zip -r $HOME/$BACKUP_DIR.zip $PROJECT_DIR
+cd $HOME
