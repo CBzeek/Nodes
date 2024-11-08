0f89149ca97000b28e673d03e63a2cc32488c9c8
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-11-08 22:08:03 +0300

    Create run.sh

---
+++ nillion/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+PROJECT_NAME="Nillion"
+
+echo ""
+echo -e "\e[1m\e[32m###########################################################################################"
+echo -e "\e[1m\e[32m### Start $PROJECT_NAME node ... \e[0m" && sleep 1
+echo ""
+docker run -d \
+  --name nillion \
+  --restart unless-stopped \
+  -v $HOME/nillion/verifier:/var/tmp nillion/verifier:v1.0.1 verify --rpc-endpoint "https://testnet-nillion-rpc.lavenderfive.com"
