7e1d9686ed5fcf7c1a00b67d21cebebd27359984
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-11-11 23:40:25 +0300

    Update and rename backup.sh to backup2.sh

--- story-protocol/iliad/backup2.sh
+++ story-protocol/iliad/backup2.sh
@@ -12,16 +12,17 @@ echo ''
 
 cd $HOME
 mkdir -p $HOME/$BACKUP_DIR/geth/iliad/geth/
-mkdir -p $HOME/$BACKUP_DIR/story/config/ $HOME/$BACKUP_DIR/story/data/
+mkdir -p $HOME/$BACKUP_DIR/$PROJECT_DIR/story/config/ $HOME/$BACKUP_DIR/$PROJECT_DIR/story/data/
 
-cp -r $HOME/$PROJECT_DIR//story/config/ $HOME/$BACKUP_DIR/story/
+cp -r $HOME/$PROJECT_DIR//story/config/ $HOME/$BACKUP_DIR/$PROJECT_DIR/story/
 rm -f $HOME/$BACKUP_DIR/story/config/addrbook.json
 rm -f $HOME/$BACKUP_DIR/story/config/genesis.json
 rm -f $HOME/$BACKUP_DIR/story/config/*.toml
 
-cp $HOME/$PROJECT_DIR/story/data/priv_validator_state.json $HOME/$BACKUP_DIR/story/data/
+cp $HOME/$PROJECT_DIR/story/data/priv_validator_state.json $HOME/$BACKUP_DIR/$PROJECT_DIR/story/data/
 
-cp $HOME/$PROJECT_DIR/geth/iliad/geth/jwtsecret $HOME/$BACKUP_DIR/geth/iliad/geth/
-cp $HOME/$PROJECT_DIR/geth/iliad/geth/nodekey $HOME/$BACKUP_DIR/geth/iliad/geth/
+cp $HOME/$PROJECT_DIR/geth/iliad/geth/jwtsecret $HOME/$BACKUP_DIR/$PROJECT_DIR/geth/iliad/geth/
+cp $HOME/$PROJECT_DIR/geth/iliad/geth/nodekey $HOME/$BACKUP_DIR/$PROJECT_DIR/geth/iliad/geth/
 
-zip -r $BACKUP_DIR.zip $HOME/$BACKUP_DIR
+cd $BACKUP_DIR
+zip -r $HOME/$BACKUP_DIR.zip $PROJECT_DIR
