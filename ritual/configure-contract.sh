aee233091228f8520299cb8ce5c0fc21f958a10f
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-11-14 21:07:22 +0300

    Create configure-contract.sh

---
+++ ritual/configure-contract.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+PROJECT_NAME="Ritual"
+
+echo ""
+echo -e "\e[1m\e[32m###########################################################################################"
+echo -e "\e[1m\e[32m### Configure $PROJECT_NAME node contract... \e[0m" && sleep 1
+echo ""
+
+read -p "Enter your Contract Address (example: 0x123....123): " CONTRACT
+
+# Backup config
+cp $HOME/infernet-container-starter/projects/hello-world/contracts/script/CallContract.s.sol $HOME/infernet-container-starter/projects/hello-world/contracts/script/CallContract.s.sol.bak
+
+# CallContract.s.sol
+sed -i "s/SaysGM saysGm *=.*/SaysGM saysGm = SaysGM(${CONTRACT});/" $HOME/infernet-container-starter/projects/hello-world/contracts/script/CallContract.s.sol
