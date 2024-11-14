0c9f8d709309185e7c30f5a7653966fae96f2439
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-11-14 16:16:53 +0300

    Update configure-node.sh

--- ritual/configure-node.sh
+++ ritual/configure-node.sh
@@ -9,32 +9,31 @@ echo ""
 read -p "Enter your private key (example: 0x123....123): " PRIVATE_KEY
 
 # Backup config
-cp $HOME/infernet-container-starter/deploy/config.json $HOME/infernet-container-starter/deploy/config.json.bak
 cp $HOME/infernet-container-starter/projects/hello-world/container/config.json $HOME/infernet-container-starter/projects/hello-world/container/config.json.bak
 cp $HOME/infernet-container-starter/deploy/docker-compose.yaml $HOME/infernet-container-starter/deploy/docker-compose.yaml.bak
 cp $HOME/infernet-container-starter/projects/hello-world/contracts/script/Deploy.s.sol $HOME/infernet-container-starter/projects/hello-world/contracts/script/Deploy.s.sol.bak
 cp $HOME/infernet-container-starter/projects/hello-world/contracts/Makefile $HOME/infernet-container-starter/projects/hello-world/contracts/Makefile.bak
 
 # RPC
-yq -i '.chain.rpc_url = "https://mainnet.base.org/"' $HOME/infernet-container-starter/deploy/config.json
+yq -i '.chain.rpc_url = "https://mainnet.base.org/"' $HOME/infernet-container-starter/projects/hello-world/container/config.json
 
 # Private Key
-PRIV=$PRIVATE_KEY yq -i '.chain.wallet.private_key = strenv(PRIV)' $HOME/infernet-container-starter/deploy/config.json
+PRIV=$PRIVATE_KEY yq -i '.chain.wallet.private_key = strenv(PRIV)' $HOME/infernet-container-starter/projects/hello-world/container/config.json
 
 # Registry address
-yq -i '.chain.registry_address = "0x3B1554f346DFe5c482Bb4BA31b880c1C18412170"' $HOME/infernet-container-starter/deploy/config.json
+yq -i '.chain.registry_address = "0x3B1554f346DFe5c482Bb4BA31b880c1C18412170"' $HOME/infernet-container-starter/projects/hello-world/container/config.json
 
 # Trail head blocks
-yq -i '.chain.trail_head_blocks = 3' $HOME/infernet-container-starter/deploy/config.json
+yq -i '.chain.trail_head_blocks = 3' $HOME/infernet-container-starter/projects/hello-world/container/config.json
 
 # Snapshot settings
-yq -i '.chain.snapshot_sync.sleep = 3' $HOME/infernet-container-starter/deploy/config.json
-yq -i '.chain.snapshot_sync.starting_sub_id = 160000' $HOME/infernet-container-starter/deploy/config.json
-yq -i '.chain.snapshot_sync.batch_size = 800' $HOME/infernet-container-starter/deploy/config.json
-yq -i '.chain.snapshot_sync.sync_period = 30' $HOME/infernet-container-starter/deploy/config.json
+yq -i '.chain.snapshot_sync.sleep = 3' $HOME/infernet-container-starter/projects/hello-world/container/config.json
+yq -i '.chain.snapshot_sync.starting_sub_id = 160000' $HOME/infernet-container-starter/projects/hello-world/container/config.json
+yq -i '.chain.snapshot_sync.batch_size = 800' $HOME/infernet-container-starter/projects/hello-world/container/config.json
+yq -i '.chain.snapshot_sync.sync_period = 30' $HOME/infernet-container-starter/projects/hello-world/container/config.json
 
 # Copy config
-cp -f $HOME/infernet-container-starter/deploy/config.json $HOME/infernet-container-starter/projects/hello-world/container/config.json
+cp -f $HOME/infernet-container-starter/projects/hello-world/container/config.json $HOME/infernet-container-starter/deploy/config.json
 
 # Infernet node image version
 yq -i '.services.node.image = "ritualnetwork/infernet-node:1.4.0"' $HOME/infernet-container-starter/deploy/docker-compose.yaml
