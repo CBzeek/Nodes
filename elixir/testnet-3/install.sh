a6aa58ad1b84c8ab87236fe3b41b821552bdd2b9
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-11-15 13:18:07 +0300

    Update install.sh

--- elixir/testnet-3/install.sh
+++ elixir/testnet-3/install.sh
@@ -34,14 +34,4 @@ echo ""
 #docker pull elixirprotocol/validator:v3
 docker pull elixirprotocol/validator:testnet --platform linux/amd64
 
-echo ""
-echo -e "\e[1m\e[32m###########################################################################################"
-echo -e "\e[1m\e[32m### Start $PROJECT_NAME node ... \e[0m" && sleep 1
-echo ""
-docker run -d \
-  --env-file $HOME/.elixir/validator.env \
-  --platform linux/amd64 \
-  --name elixir \
-  --restart unless-stopped \
-  -p 17690:17690 \
-  elixirprotocol/validator:testnet
+source <(wget -qO- 'https://raw.githubusercontent.com/CBzeek/Nodes/refs/heads/main/elixir/testnet-3/run.sh')
