e9740f80ec20bf886682ceedb373f1cd8969b113
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-11-19 23:19:46 +0300

    Update server-xfce.sh

--- !tools/server-xfce.sh
+++ !tools/server-xfce.sh
@@ -41,5 +41,25 @@ sudo systemctl status xrdp
 
 sudo ufw allow 3389
 
+sudo snap remove firefox
+
+sudo install -d -m 0755 /etc/apt/keyrings
+
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+
+echo '
+Package: *
+Pin: origin packages.mozilla.org
+Pin-Priority: 1000
+
+Package: firefox*
+Pin: release o=Ubuntu
+Pin-Priority: -1' | sudo tee /etc/apt/preferences.d/mozilla
+
+sudo apt update && sudo apt remove firefox
+
+sudo apt install firefox
 
 
