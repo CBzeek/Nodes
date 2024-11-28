ede653feac58e4755ef2bec018927c2a6c9cc706
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-11-28 22:46:58 +0300

    Create snapshot.sh

---
+++ gitopia/mainnet/snapshot.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+PROJECT_NAME="Gitopia"
+
+
+echo -e "\e[1m\e[32m###########################################################################################"
+echo -e "\e[1m\e[32m### Stopping $PROJECT_NAME node... \e[0m" && sleep 1
+echo ''
+sudo systemctl stop gitopiad
+
+
+echo -e "\e[1m\e[32m###########################################################################################"
+echo -e "\e[1m\e[32m### Downloading $PROJECT_NAME node snapshot... \e[0m" && sleep 1
+echo ''
+rm -rf $HOME/.gitopia/data/application.db 
+sleep 1
+
+cp $HOME/.gitopia/data/priv_validator_state.json $HOME/.gitopia/priv_validator_state.json.backup
+gitopiad tendermint unsafe-reset-all --home $HOME/.gitopia --keep-addr-book
+curl https://snapshots.nodejumper.io/gitopia/gitopia_latest.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.gitopia
+mv $HOME/.gitopia/priv_validator_state.json.backup $HOME/.gitopia/data/priv_validator_state.json
+
+
+echo -e "\e[1m\e[32m###########################################################################################"
+echo -e "\e[1m\e[32m### Restarting $PROJECT_NAME node... \e[0m" && sleep 1
+echo ''
+sudo systemctl restart gitopiad
