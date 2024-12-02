c3d23f3ba347178b62693442c4880f938b91be3b
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-12-02 20:57:54 +0300

    Create combo-update.sh

---
+++ story-protocol/odyssey/combo-update.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+PROJECT_NAME="Story Protocol"
+
+echo ''
+echo -e "\e[1m\e[32m###########################################################################################"
+echo -e "\e[1m\e[32m### Update $PROJECT_NAME node bin... \e[0m" && sleep 1
+echo ''
+source <(wget -qO- 'https://raw.githubusercontent.com/CBzeek/Nodes/refs/heads/main/story-protocol/odyssey/update-bin.sh')
+
+
+echo ""
+echo -e "\e[1m\e[32m###########################################################################################"
+echo -e "\e[1m\e[32m### Get $PROJECT_NAME node snapshot... \e[0m" && sleep 1
+echo ""
+source <(wget -qO- 'https://raw.githubusercontent.com/CBzeek/Nodes/refs/heads/main/story-protocol/odyssey/snapshot.sh')
+
+
+echo ""
+echo -e "\e[1m\e[32m###########################################################################################"
+echo -e "\e[1m\e[32m### Show $PROJECT_NAME node logs... \e[0m" && sleep 1
+echo ""
+sudo journalctl -u story -f -o cat
