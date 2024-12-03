1cdb287742c555b566b8567cdcdbeb4da0ec0dc7
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-12-04 00:09:00 +0300

    Create backup.sh

---
+++ gitopia/mainnet/backup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+PROJECT_NAME="Gitopia"
+PROJECT_DIR=".gitopia"
+BACKUP_DIR=backup_$(curl -s eth0.me)_$(date +%F--%H-%M-%S)
+
+sudo apt install rsync zip -y
+
+echo ''
+echo -e "\e[1m\e[32m###########################################################################################"
+echo -e "\e[1m\e[32m### Backup $PROJECT_NAME node configuration files... \e[0m" && sleep 1
+echo ''
+
+cd $HOME
+mkdir -p $BACKUP_DIR/gitopia/data/
+cp $HOME/$PROJECT_DIR/data/*.json $BACKUP_DIR/gitopia/data/
+rsync -av --exclude='data' --exclude='log' $HOME/$PROJECT_DIR/ $BACKUP_DIR/gitopia/
+
+zip -r $BACKUP_DIR.zip $HOME/$BACKUP_DIR
