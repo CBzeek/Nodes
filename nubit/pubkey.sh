898f7c9d9f39c7d9379f0421bde3f5b91adfd6b6
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-12-19 12:49:30 +0300

    Update pubkey.sh

--- nubit/pubkey.sh
+++ nubit/pubkey.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 PROJECT_NAME="Nubit"
 
+
+# Logo
+source <(wget -qO- 'https://raw.githubusercontent.com/CBzeek/Nodes/refs/heads/main/!tools/logo.sh')
+
+
 echo ""
 echo -e "\e[1m\e[32m###########################################################################################"
 echo -e "\e[1m\e[32m### Showing $PROJECT_NAME node pubkey... \e[0m" && sleep 1
