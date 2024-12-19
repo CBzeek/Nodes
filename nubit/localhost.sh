9970c58f00e18a1020bce7ceda7bd7b6e0f22bd8
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-12-19 12:49:40 +0300

    Update localhost.sh

--- nubit/localhost.sh
+++ nubit/localhost.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 PROJECT_NAME="Nubit"
 
+# Logo
+source <(wget -qO- 'https://raw.githubusercontent.com/CBzeek/Nodes/refs/heads/main/!tools/logo.sh')
+
+
 echo ""
 echo -e "\e[1m\e[32m###########################################################################################"
 echo -e "\e[1m\e[32m### Adding $PROJECT_NAME node localhost settings... \e[0m" && sleep 1
