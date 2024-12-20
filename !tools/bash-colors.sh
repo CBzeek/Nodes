730182ddb95fa67f8a2856875994fa35645fa469
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-12-21 02:48:03 +0300

    Update bash-colors.sh

--- !tools/bash-colors.sh
+++ !tools/bash-colors.sh
@@ -5,3 +5,15 @@ B_GREEN='\033[1;32m'       # Bold Green
 B_YELLOW='\033[1;33m'      # Yellow
 B_RED='\033[1;31m'         # Bold Red
 NO_COLOR='\033[0m'         # Color Reset
+
+
+
+# Import Colors
+# source <(wget -qO- 'https://raw.githubusercontent.com/CBzeek/Nodes/refs/heads/main/!tools/bash-colors.sh')
+
+#echo ""
+#echo -e "${B_GREEN}"
+#echo -e "${B_RED}"
+#echo -e "${B_YELLOW}"
+#echo -e "${NO_COLOR}"
+#echo "" && sleep 2
