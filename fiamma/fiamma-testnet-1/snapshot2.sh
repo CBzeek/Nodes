0a8d96aaad3e0c8073679107dbfd688049a38c28
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-12-21 05:13:33 +0300

    Update and rename snapshot.sh to snapshot2.sh

--- fiamma/fiamma-testnet-1/snapshot2.sh
+++ fiamma/fiamma-testnet-1/snapshot2.sh
@@ -26,7 +26,7 @@ rm -rf $HOME/.fiamma/data/application.db
 cp $HOME/.fiamma/data/priv_validator_state.json $HOME/.fiamma/priv_validator_state.json.backup
 fiammad tendermint unsafe-reset-all --home $HOME/.fiamma --keep-addr-book
 curl https://server-5.itrocket.net/testnet/fiamma/fiamma_2024-11-26_310597_snap.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.fiamma
-mv $HOME/.fiamma/priv_validator_state.json.backup $HOME/.wfiamma/data/priv_validator_state.json
+mv $HOME/.fiamma/priv_validator_state.json.backup $HOME/.fiamma/data/priv_validator_state.json
 #rm -f ./latest_snapshot.tar.lz4
 
 
