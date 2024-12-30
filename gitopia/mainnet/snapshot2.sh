7279759316ae44c08d21b1476693934b4e7bd2dc
CBzeek <60964131+CBzeek@users.noreply.github.com>
2024-12-30 13:12:15 +0300

    Update and rename snapshot.sh to snapshot2.sh

--- gitopia/mainnet/snapshot2.sh
+++ gitopia/mainnet/snapshot2.sh
@@ -17,7 +17,9 @@ sleep 1
 cp $HOME/.gitopia/data/priv_validator_state.json $HOME/.gitopia/priv_validator_state.json.backup
 gitopiad tendermint unsafe-reset-all --home $HOME/.gitopia --keep-addr-book
 #curl https://snapshots.nodejumper.io/gitopia/gitopia_latest.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.gitopia
-curl -o - -L https://gitopia.snapshot.stavr.tech/gitopia-snap.tar.lz4 | lz4 -c -d - | tar -x -C $HOME/.gitopia --strip-components 2
+#curl -o - -L https://gitopia.snapshot.stavr.tech/gitopia-snap.tar.lz4 | lz4 -c -d - | tar -x -C $HOME/.gitopia --strip-components 2
+LATEST_SNAPSHOT=$(curl -s https://server-1.stavr.tech/Mainnet/Gitopia/ | grep -oE 'gitopia-snap-[0-9]+\.tar\.lz4' | while read SNAPSHOT; do HEIGHT=$(curl -s "https://server-1.stavr.tech/Mainnet/Gitopia/${SNAPSHOT%.tar.lz4}-info.txt" | awk '/Block height:/ {print $3}'); echo "$SNAPSHOT $HEIGHT"; done | sort -k2 -nr | head -n 1 | awk '{print $1}')
+curl -o - -L https://server-1.stavr.tech/Mainnet/Gitopia/$LATEST_SNAPSHOT | lz4 -c -d - | tar -x -C $HOME/.gitopia
 mv $HOME/.gitopia/priv_validator_state.json.backup $HOME/.gitopia/data/priv_validator_state.json
 
 
