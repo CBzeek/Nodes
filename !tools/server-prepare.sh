e67d3741d050e948db169d8e6cd05bc7327a5f38
CBzeek <60964131+CBzeek@users.noreply.github.com>
2025-02-15 04:46:43 +0300

    Update server-prepare.sh

--- !tools/server-prepare.sh
+++ !tools/server-prepare.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
+# Import Colors
+source <(wget -qO- 'https://raw.githubusercontent.com/CBzeek/Nodes/refs/heads/main/!tools/bash-colors.sh')
+
+print_header() {
+  echo ""
+  echo -e "${B_GREEN}"
+  echo -e "###########################################################################################"
+  echo -e "### ${B_YELLOW}$1"
+  echo -e "${NO_COLOR}" && sleep 1
+  echo ""
+}
 
 cd $HOME
 
 #Ubuntu update and upgrade
-echo ''
-echo -e "\e[1m\e[32m###########################################################################################"
-echo -e "\e[1m\e[32m### Updating and Upgrading server... \e[0m"
-echo '' && sleep 1
+print_header "Updating and Upgrading server..."
 sudo apt update && sudo apt upgrade -y
 
 
 #Install software
-echo ''
-echo -e "\e[1m\e[32m###########################################################################################"
-echo -e "\e[1m\e[32m### Installing dependencies to server... \e[0m"
-echo '' && sleep 1
+print_header "Installing dependencies to server..."
 sudo apt install curl mc git jq screen lz4 build-essential htop zip unzip wget rsync snapd -y
 sudo snap install yq
