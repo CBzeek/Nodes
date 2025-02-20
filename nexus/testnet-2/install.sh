5f9c3b7ada5fb36db5c158142ac45ab0dee6ce2f
CBzeek <60964131+CBzeek@users.noreply.github.com>
2025-02-20 16:22:59 +0300

    Create install.sh

---
+++ nexus/testnet-2/install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Variables
+PROJECT_NAME="Nexus"
+
+# Logo
+source <(wget -qO- 'https://raw.githubusercontent.com/CBzeek/Nodes/refs/heads/main/!tools/logo.sh')
+
+# Import Colors
+source <(wget -qO- 'https://raw.githubusercontent.com/CBzeek/Nodes/refs/heads/main/!tools/bash-colors.sh')
+
+
+print_header "Installing $PROJECT_NAME node..."
+
+# Install denepdencies
+sudo apt install build-essential pkg-config libssl-dev git-all -y
+sudo apt install -y protobuf-compiler
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+rustup target add riscv32i-unknown-none-elf
+
+source "$HOME/.cargo/env"
+
+sudo apt install screen -y && screen -Rd nexus5
+
+curl https://cli.nexus.xyz/ | sh
+
+
