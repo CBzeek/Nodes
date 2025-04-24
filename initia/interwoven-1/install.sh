6207a5bc06261c49348012c11ed95682ac061d7e
CBzeek <60964131+CBzeek@users.noreply.github.com>
2025-04-25 01:55:40 +0300

    Create install.sh

---
+++ initia/interwoven-1/install.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+PROJECT_NAME="Initia"
+
+# Logo
+source <(wget -qO- 'https://raw.githubusercontent.com/CBzeek/Nodes/refs/heads/main/!tools/logo.sh')
+
+
+if [ ! $MONIKER ]; then
+    echo ""
+    echo -e "\e[1m\e[32m###########################################################################################"
+    echo -e "\e[1m\e[32m### Setting $PROJECT_NAME node moniker... \e[0m" && sleep 1
+    echo ""
+    read -p "Enter node moniker: " MONIKER
+    echo 'export MONIKER='\"${MONIKER}\" >> ~/.bash_profile
+fi
+
+#read -p "Enter node moniker: " MONIKER
+#echo 'export MONIKER='\"${MONIKER}\" >> ~/.bash_profile
+echo 'export CHAIN_ID="interwoven-1"' >> ~/.bash_profile
+echo 'export WALLET_NAME="wallet"' >> ~/.bash_profile
+source $HOME/.bash_profile
+
+
+echo ""
+echo -e "\e[1m\e[32m###########################################################################################"
+echo -e "\e[1m\e[32m### Installing dependencies... \e[0m" && sleep 1
+echo ""
+#update
+sudo apt update && \
+sudo apt install curl git jq build-essential gcc unzip wget lz4 -y
+
+#install go
+cd $HOME && \
+ver="1.21.3" && \
+wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz" && \
+sudo rm -rf /usr/local/go && \
+sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz" && \
+rm "go$ver.linux-amd64.tar.gz" && \
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile && \
+source ~/.bash_profile && \
+go version
+
+
+echo ""
+echo -e "\e[1m\e[32m###########################################################################################"
+echo -e "\e[1m\e[32m### Installing $PROJECT_NAME node... \e[0m" && sleep 1
+echo ""
+#install binary 
+git clone https://github.com/initia-labs/initia.git
+cd initia
+git checkout v1.0.0
+make install
+
+#init node
+cd $HOME
+initiad init $MONIKER --chain-id $CHAIN_ID
+
+#get genesis
+wget -O $HOME/.initia/config/genesis.json https://initia.s3.ap-southeast-1.amazonaws.com/initiation-1/genesis.json 
+
+#get addrbook
+wget -O $HOME/.initia/config/addrbook.json https://initia.s3.ap-southeast-1.amazonaws.com/initiation-1/addrbook.json
+
+#peers, seeds
+source <(wget -qO- 'https://raw.githubusercontent.com/CBzeek/Nodes/main/initia/initiation-1/peers.sh')
+
+#set pruning
+sed -i \
+    -e "s/^pruning *=.*/pruning = \"custom\"/" \
+    -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"100\"/" \
+    -e "s/^pruning-interval *=.*/pruning-interval = \"10\"/" \
+    "$HOME/.initia/config/app.toml"
+
+#set gas
+sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.15uinit,0.01uusdc\"/" $HOME/.initia/config/app.toml
+
+
+#echo ""
+#echo -e "\e[1m\e[32m###########################################################################################"
+#echo -e "\e[1m\e[32m### Downloading $PROJECT_NAME node snapshot... \e[0m" && sleep 1
+#echo ""
+#shapshot
+#cp $HOME/.initia/data/priv_validator_state.json $HOME/.initia/priv_validator_state.json.backup
+#initiad tendermint unsafe-reset-all --home $HOME/.initia --keep-addr-book
+##rm -rf $HOME/.initia/data
+##curl -L https://snapshots.kzvn.xyz/initia/initiation-1_latest.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.initia
+#curl -L https://snapshots-testnet.nodejumper.io/initia-testnet/initia-testnet_latest.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.initia
+#mv $HOME/.initia/priv_validator_state.json.backup $HOME/.initia/data/priv_validator_state.json
+#rm -f ./initia-testnet_latest.tar.lz4
+
+
+
+echo ""
+echo -e "\e[1m\e[32m###########################################################################################"
+echo -e "\e[1m\e[32m### Setting $PROJECT_NAME node service... \e[0m" && sleep 1
+echo ""
+#service file
+sudo tee /etc/systemd/system/initiad.service > /dev/null <<EOF
+[Unit]
+Description=initia node
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$(which initiad) start
+Restart=on-failure
+RestartSec=5
+LimitNOFILE=10000
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
+echo ""
+echo -e "\e[1m\e[32m###########################################################################################"
+echo -e "\e[1m\e[32m### Starting $PROJECT_NAME node... \e[0m" && sleep 1
+echo ""
+#start node
+sudo systemctl daemon-reload
+sudo systemctl enable initiad 
+sudo systemctl start initiad
