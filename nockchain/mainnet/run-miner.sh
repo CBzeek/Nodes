a222fedc6dcfc3fb21aefecdf6506eea306445ab
CBzeek <60964131+CBzeek@users.noreply.github.com>
2025-06-04 17:00:14 +0300

    Update run-miner.sh

--- nockchain/mainnet/run-miner.sh
+++ nockchain/mainnet/run-miner.sh
@@ -8,34 +8,37 @@ source <(wget -qO- 'https://raw.githubusercontent.com/CBzeek/Nodes/refs/heads/ma
 # Import Colors
 source <(wget -qO- 'https://raw.githubusercontent.com/CBzeek/Nodes/refs/heads/main/!tools/bash-colors.sh')
 
-source $HOME/nockchain/.env
 
-RUST_BACKTRACE=1
-
-export RUST_LOG
-export MINIMAL_LOG_FORMAT
-export MINING_PUBKEY
-export RUST_BACKTRACE
-
-cd $HOME/nockchain
-
-if [ -n "$1" ]
-then
-    # Run additional miner
-    print_header "$PROJECT_NAME additional miner $1 start..."
-    sudo sysctl -w vm.overcommit_memory=1
-    mkdir -p miner-node$1
-    cd miner-node$1
-    rm -f nockchain.sock
-    nockchain --npc-socket nockchain.sock --mining-pubkey ${MINING_PUBKEY} --mine
-else
-    # Run main miner
-    print_header "$PROJECT_NAME main miner start..."
-    sudo sysctl -w vm.overcommit_memory=1
-    mkdir -p miner-node 
-    cd miner-node 
-    rm -f nockchain.sock 
-    nockchain --npc-socket nockchain.sock --mining-pubkey ${MINING_PUBKEY} --mine
-fi
-
+while true
+do
+    source $HOME/nockchain/.env
+    
+    RUST_BACKTRACE=1
+    
+    export RUST_LOG
+    export MINIMAL_LOG_FORMAT
+    export MINING_PUBKEY
+    export RUST_BACKTRACE
+    
+    cd $HOME/nockchain
+    
+    if [ -n "$1" ]
+    then
+        # Run additional miner
+        print_header "$PROJECT_NAME additional miner $1 start..."
+        sudo sysctl -w vm.overcommit_memory=1
+        mkdir -p miner-node$1
+        cd miner-node$1
+        rm -f nockchain.sock
+        nockchain --npc-socket nockchain.sock --mining-pubkey ${MINING_PUBKEY} --mine
+    else
+        # Run main miner
+        print_header "$PROJECT_NAME main miner start..."
+        sudo sysctl -w vm.overcommit_memory=1
+        mkdir -p miner-node 
+        cd miner-node 
+        rm -f nockchain.sock 
+        nockchain --npc-socket nockchain.sock --mining-pubkey ${MINING_PUBKEY} --mine
+    fi
+done
 
