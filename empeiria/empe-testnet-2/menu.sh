2b7492072ab43d19384a14973ede9ec56fc56a10
CBzeek <60964131+CBzeek@users.noreply.github.com>
2025-06-28 11:50:00 +0300

    Update and rename menu2.sh to menu.sh

--- empeiria/empe-testnet-2/menu.sh
+++ empeiria/empe-testnet-2/menu.sh
@@ -50,6 +50,7 @@ node_install() {
   sudo mv -f libwasmvm.x86_64.so /usr/lib/libwasmvm.x86_64.so
 
   # Install binary 
+  cd $HOME 
   mkdir $WORK_DIR
   cd $HOME/$WORK_DIR
   curl -LO https://github.com/empe-io/empe-chain-releases/raw/master/${VERSION}/emped_${VERSION}_linux_amd64.tar.gz
@@ -72,31 +73,30 @@ node_install() {
   # Get addrbook
   wget -O $HOME/.empe-chain/config/addrbook.json https://server-5.itrocket.net/testnet/empeiria/addrbook.json
   
-
   # Set peers, seeds
   SEEDS="20ca5fc4882e6f975ad02d106da8af9c4a5ac6de@empeiria-testnet-seed.itrocket.net:28656"
   PEERS="03aa072f917ed1b79a14ea2cc660bc3bac787e82@empeiria-testnet-peer.itrocket.net:28656,e058f20874c7ddf7d8dc8a6200ff6c7ee66098ba@65.109.93.124:29056,4c055c7272b26141c26c82334d07cc65558e79aa@134.255.182.245:14656,78f766310a83b6670023169b93f01d140566db79@65.109.83.40:29056,0f87465704e95ea92f6906f47a99a5d91aff0e1c@195.201.86.60:43656,e62b549646fee135cf010bc10641f728aba7fbd0@65.108.234.158:26626,2db322b41d26559476f929fda51bce06c3db8ba4@65.109.24.155:11256,080d9cc12e08fb64dd0f4528d0da4a84d5d9428e@37.27.83.234:26656,810e21adee3b8f337bab0df70ba75d38afde2348@152.53.0.11:29656,5fc98f2ec4b2a6001aa5655c9852d259e83a8e74@65.108.226.44:11256,45bdc8628385d34afc271206ac629b07675cd614@65.21.202.124:25656"
   sed -i -e "/^\[p2p\]/,/^\[/{s/^[[:space:]]*seeds *=.*/seeds = \"$SEEDS\"/}" \
-         -e "/^\[p2p\]/,/^\[/{s/^[[:space:]]*persistent_peers *=.*/persistent_peers = \"$PEERS\"/}" $HOME/$WORK_DIR/config/config.toml
+         -e "/^\[p2p\]/,/^\[/{s/^[[:space:]]*persistent_peers *=.*/persistent_peers = \"$PEERS\"/}" $HOME/.empe-chain/config/config.toml
 
   # Set pruning
   pruning="custom"
   pruning_keep_recent="100"
   pruning_interval="19"
-  sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" $HOME/$WORK_DIR/config/app.toml
-  sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" $HOME/$WORK_DIR/config/app.toml
-  sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/$WORK_DIR/config/app.toml
+  sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" $HOME/.empe-chain/config/app.toml
+  sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" $HOME/.empe-chain/config/app.toml
+  sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/.empe-chain/config/app.toml
 
   # Set minimum gas
   minimum_gas="0.0001uempe"
-  sed -i "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"$minimum_gas\"/" $HOME/$WORK_DIR/config/app.toml
+  sed -i "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"$minimum_gas\"/" $HOME/.empe-chain/config/app.toml
   
   # Disable indexing
   indexer="null"
-  sed -i -e "s/^indexer *=.*/indexer = \"$indexer\"/" $HOME/$WORK_DIR/config/config.toml
+  sed -i -e "s/^indexer *=.*/indexer = \"$indexer\"/" $HOME/.empe-chain/config/config.toml
   
   # Enable prometheus
-  sed -i -e "s/prometheus = false/prometheus = true/" $HOME/$WORK_DIR/config/config.toml
+  sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.empe-chain/config/config.toml
 
   # Create service file
   print_header "Setting $PROJECT_NAME node service..."
@@ -107,8 +107,8 @@ Description=Empeiria node
 After=network-online.target
 [Service]
 User=$USER
-WorkingDirectory=$HOME/$WORK_DIR
-ExecStart=$(which emped) start --home $HOME/$WORK_DIR
+WorkingDirectory=$HOME/.empe-chain
+ExecStart=$(which emped) start --home $HOME/.empe-chain
 Restart=on-failure
 RestartSec=5
 LimitNOFILE=65535
