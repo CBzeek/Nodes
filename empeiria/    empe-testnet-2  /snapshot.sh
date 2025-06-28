419c03fbc36622f456db994c2ce1559b6c66c1a8
CBzeek <60964131+CBzeek@users.noreply.github.com>
2025-06-28 10:46:13 +0300

    Create snapshot.sh

---
+++ empeiria/    empe-testnet-2  /snapshot.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Logo
+source <(wget -qO- 'https://raw.githubusercontent.com/CBzeek/Nodes/refs/heads/main/!tools/logo.sh')
+
+# Import Colors
+source <(wget -qO- 'https://raw.githubusercontent.com/CBzeek/Nodes/refs/heads/main/!tools/bash-colors.sh')
+
+# Variables
+PROJECT_NAME="Empeiria"
+
