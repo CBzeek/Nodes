e68fa3d92b38938fbb87ea69cd9d1f0b5e31d4e9
CBzeek <60964131+CBzeek@users.noreply.github.com>
2025-06-28 11:49:53 +0300

    Update snapshot.sh

--- empeiria/empe-testnet-2/snapshot.sh
+++ empeiria/empe-testnet-2/snapshot.sh
@@ -8,3 +8,10 @@ source <(wget -qO- 'https://raw.githubusercontent.com/CBzeek/Nodes/refs/heads/ma
 # Variables
 PROJECT_NAME="Empeiria"
 
+
+sudo systemctl stop emped
+cp $HOME/.empe-chain/data/priv_validator_state.json $HOME/.empe-chain/priv_validator_state.json.backup
+rm -rf $HOME/.empe-chain/data $HOME/.empe-chain/wasm
+curl https://server-5.itrocket.net/testnet/empeiria/empeiria_2025-06-28_5567891_snap.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.empe-chain
+mv $HOME/.empe-chain/priv_validator_state.json.backup $HOME/.empe-chain/data/priv_validator_state.json
+sudo systemctl restart emped
