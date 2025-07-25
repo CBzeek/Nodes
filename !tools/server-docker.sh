963f587e57cffabb2983eb8499713c98374d7d41
CBzeek <60964131+CBzeek@users.noreply.github.com>
2025-07-25 20:58:35 +0300

    Update server-docker.sh

--- !tools/server-docker.sh
+++ !tools/server-docker.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
+
+# Import Colors
+source <(wget -qO- 'https://raw.githubusercontent.com/CBzeek/Nodes/refs/heads/main/!tools/bash-colors.sh')
+
 cd $HOME
 
-echo ""
-echo -e "\e[1m\e[32m###########################################################################################"
-echo -e "\e[1m\e[32m### Install dependencies... \e[0m" && sleep 1
-echo ""
+#Install Software
+print_header "Install dependencies..."
 sudo apt update
 sudo apt install curl -y
 
 
 #Install Docker
-echo ''
-echo -e "\e[1m\e[32m###########################################################################################"
-echo -e "\e[1m\e[32m### Install Docker to server... \e[0m" && sleep 1
-echo ''
+print_header "Install Docker to server..."
 curl -fsSL https://get.docker.com -o get-docker.sh 
 sudo sh get-docker.sh
