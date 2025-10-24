a084c066789c8d09f1935c2745f747856aa9de45
CBzeek <60964131+CBzeek@users.noreply.github.com>
2025-10-24 17:54:10 +0000

    Update and rename snapshot.sh to snapshot2.sh

--- empeiria/empe-testnet-2/snapshot2.sh
+++ empeiria/empe-testnet-2/snapshot2.sh
@@ -7,7 +7,7 @@ source <(wget -qO- 'https://raw.githubusercontent.com/CBzeek/Nodes/refs/heads/ma
 
 # Variables
 PROJECT_NAME="Empeiria"
-
+SNAPSHOT_URL="https://server-1.itrocket.net/testnet/empeiria/"
 
 # Stop node
 print_header "Stopping $PROJECT_NAME node..."
@@ -21,8 +21,10 @@ cd $HOME
 rm -rf $HOME/.empe-chain/data/application.db
 cp $HOME/.empe-chain/data/priv_validator_state.json $HOME/.empe-chain/priv_validator_state.json.backup
 emped tendermint unsafe-reset-all --home $HOME/.empe-chain --keep-addr-book
-LATEST_SNAPSHOT=$(curl -s  https://server-1.itrocket.net/testnet/empeiria/ | grep -oE 'empeiria_[0-9-]+_([0-9]+)_snap\.tar\.lz4' | sort -t'_' -k3 -n | tail -1)
-curl -S https://server-1.itrocket.net/testnet/empeiria/$LATEST_SNAPSHOT | lz4 -dc - | tar -xf - -C $HOME/.empe-chain
+# LATEST_SNAPSHOT=$(curl -s  https://server-1.itrocket.net/testnet/empeiria/ | grep -oE 'empeiria_[0-9-]+_([0-9]+)_snap\.tar\.lz4' | sort -t'_' -k3 -n | tail -1)
+# curl -S https://server-1.itrocket.net/testnet/empeiria/$LATEST_SNAPSHOT | lz4 -dc - | tar -xf - -C $HOME/.empe-chain
+LATEST_SNAPSHOT=$(curl -s  $SNAPSHOT_URL | grep -oE 'empeiria_[0-9-]+_([0-9]+)_snap\.tar\.lz4' | sort -t'_' -k3 -n | tail -1)
+curl -S $SNAPSHOT_URL$LATEST_SNAPSHOT | lz4 -dc - | tar -xf - -C $HOME/.empe-chain
 mv $HOME/.empe-chain/priv_validator_state.json.backup $HOME/.empe-chain/data/priv_validator_state.json
 rm -f ./$LATEST_SNAPSHOT
 
