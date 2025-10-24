8d21bece64160cfb6d1602417b3c49fef1f9c6ad
CBzeek <60964131+CBzeek@users.noreply.github.com>
2025-10-24 17:52:15 +0000

    Update and rename snapshot.sh to snapshot2.sh

--- empeiria/empe-testnet-2/snapshot2.sh
+++ empeiria/empe-testnet-2/snapshot2.sh
@@ -22,7 +22,7 @@ rm -rf $HOME/.empe-chain/data/application.db
 cp $HOME/.empe-chain/data/priv_validator_state.json $HOME/.empe-chain/priv_validator_state.json.backup
 emped tendermint unsafe-reset-all --home $HOME/.empe-chain --keep-addr-book
 LATEST_SNAPSHOT=$(curl -s  https://server-1.itrocket.net/testnet/empeiria/ | grep -oE 'empeiria_[0-9-]+_([0-9]+)_snap\.tar\.lz4' | sort -t'_' -k3 -n | tail -1)
-curl -S https://server-5.itrocket.net/testnet/empeiria/$LATEST_SNAPSHOT | lz4 -dc - | tar -xf - -C $HOME/.empe-chain
+curl -S https://server-1.itrocket.net/testnet/empeiria/$LATEST_SNAPSHOT | lz4 -dc - | tar -xf - -C $HOME/.empe-chain
 mv $HOME/.empe-chain/priv_validator_state.json.backup $HOME/.empe-chain/data/priv_validator_state.json
 rm -f ./$LATEST_SNAPSHOT
 
